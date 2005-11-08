5e5bb81d481579c46bd4d44afa03b939d35cd5cf
shot6 <shinpei.ohtani@gmail.com>
2005-11-08 16:31:19 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@14 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/test/java/javax/faces/application/TestStateManager.java
@@ -0,0 +1,71 @@
+package javax.faces.application;
+
+import java.io.IOException;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.mock.MockExternalContext;
+import javax.faces.mock.MockFacesContext;
+import javax.servlet.ServletContext;
+
+import org.seasar.extension.unit.S2TestCase;
+import org.seasar.framework.aop.interceptors.MockInterceptor;
+
+
+public class TestStateManager extends S2TestCase {
+
+    public TestStateManager(String name) {
+        super(name);
+    }
+
+    public void testIsSavingStateClient() {
+        
+        StateManager stateManager = new StateManagerImpl();
+        try{
+            stateManager.isSavingStateClient(null);
+            fail();
+        }catch(NullPointerException e){
+            assertTrue(true);
+        }
+
+        MockInterceptor mi = new MockInterceptor();
+        mi.setReturnValue("getInitParameter", StateManager.STATE_SAVING_METHOD_CLIENT);
+        ServletContext servletContext = (ServletContext)mi.createProxy(ServletContext.class);
+        MockExternalContext externalContext = new MockExternalContext(servletContext);
+        MockFacesContext context = new MockFacesContext(externalContext);
+        assertTrue(stateManager.isSavingStateClient(context));
+    }
+
+    private static class StateManagerImpl extends StateManager{
+
+        public SerializedView saveSerializedView(FacesContext context) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void writeState(FacesContext context, SerializedView state) throws IOException {
+            throw new UnsupportedOperationException();
+        }
+
+        protected Object getTreeStructureToSave(FacesContext context) {
+            throw new UnsupportedOperationException();
+        }
+
+        protected Object getComponentStateToSave(FacesContext context) {
+            throw new UnsupportedOperationException();
+        }
+
+        public UIViewRoot restoteView(FacesContext context, String viewId) {
+            throw new UnsupportedOperationException();
+        }
+
+        protected UIViewRoot restoreTreeStructure(FacesContext context, String viewId) {
+            throw new UnsupportedOperationException();
+        }
+
+        protected void restoreComponentState(FacesContext context, UIViewRoot viewRoot) {
+            throw new UnsupportedOperationException();
+        }
+        
+    }
+
+}
