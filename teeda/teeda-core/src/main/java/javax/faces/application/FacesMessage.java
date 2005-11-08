1e8c05a1893f37e8fa67fc433b06a8200f98fb35
shot6 <shinpei.ohtani@gmail.com>
2005-11-08 16:25:03 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@10 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/javax/faces/application/FacesMessage.java
+++ teeda/teeda-core/src/main/java/javax/faces/application/FacesMessage.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package javax.faces.application;
 
 import java.io.Serializable;
@@ -7,9 +22,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author Shinpei Ohtani
+ */
 public class FacesMessage implements Serializable{
 
-	public static final String FACES_MESSAGES = "javax.faces.Messages";
+	private static final long serialVersionUID = 3258125843346766128L;
+
+    public static final String FACES_MESSAGES = "javax.faces.Messages";
 
 	public static final Severity SEVERITY_INFO = new Severity("INFO", 1);
 
--- teeda/teeda-core/src/main/java/javax/faces/application/StateManager.java
+++ teeda/teeda-core/src/main/java/javax/faces/application/StateManager.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package javax.faces.application;
 
 import java.io.IOException;
@@ -6,6 +21,9 @@ import java.io.Serializable;
 import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
 
+/**
+ * @author Shinpei Ohtani
+ */
 public abstract class StateManager {
 
 	public static final String STATE_SAVING_METHOD_CLIENT = "client";
@@ -46,7 +64,9 @@ public abstract class StateManager {
 
 	public class SerializedView implements Serializable{
 
-		private Object structure_ = null;
+		private static final long serialVersionUID = 3256443616359823160L;
+
+        private Object structure_ = null;
 
 		private Object state_ = null;
 
