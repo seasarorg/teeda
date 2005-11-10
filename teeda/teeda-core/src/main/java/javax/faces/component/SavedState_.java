b1ab462df61e152b5a5aaa742314d9d951ec8817
shot6 <shinpei.ohtani@gmail.com>
2005-11-10 14:45:36 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@30 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/javax/faces/component/SavedState_.java
+++ teeda/teeda-core/src/main/java/javax/faces/component/SavedState_.java
@@ -1,11 +1,30 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package javax.faces.component;
 
 import java.io.Serializable;
 
-
+/**
+ * @author Shinpei Ohtani
+ */
 class SavedState_ implements Serializable{
 
-	private Object localeValue_ = null;
+	private static final long serialVersionUID = 3688791349478568242L;
+
+    private Object localeValue_ = null;
 	
 	private Object submittedValue_ = null;
 	
@@ -25,6 +44,7 @@ class SavedState_ implements Serializable{
 	public boolean isLocaleValueSet() {
 		return localeValueSet_;
 	}
+    
 	public void setLocaleValueSet(boolean localeValueSet) {
 		localeValueSet_ = localeValueSet;
 	}
