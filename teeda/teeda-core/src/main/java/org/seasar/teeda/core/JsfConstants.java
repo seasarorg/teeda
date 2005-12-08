943b910686514007a63c8b673ca3123fea5c5da6
shot6 <shinpei.ohtani@gmail.com>
2005-12-08 17:05:20 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@53 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/JsfConstants.java
@@ -0,0 +1,38 @@
+package org.seasar.teeda.core;
+
+
+public interface JsfConstants {
+
+    public static final String MESSAGES = "Messages";
+
+    public static final String DEFAULT_XML_NS = "http://java.sun.com/JSF/Configuration";
+    
+    public static final String CORE_PACKAGE_ROOT = "org.seasar.teeda.core";
+    
+    public static final String APPLICATION_SCOPE = "applicationScope";
+
+    public static final String COOKIE = "cookie";
+
+    public static final String FACES_CONTEXT = "facesContext";
+
+    public static final String HEADER = "header";
+
+    public static final String HEADER_VALUES = "headerValues";
+
+    public static final String INIT_PARAM = "initParam";
+
+    public static final String PARAM = "param";
+
+    public static final String PARAM_VALUES = "paramValues";
+
+    public static final String REQUEST_SCOPE = "requestScope";
+
+    public static final String SESSION_SCOPE = "sessionScope";
+
+    public static final String VIEW = "view";
+
+    public static final String[] JSF_IMPLICIT_OBJECTS = {
+        APPLICATION_SCOPE, COOKIE, FACES_CONTEXT, HEADER, HEADER_VALUES, 
+        INIT_PARAM, PARAM, PARAM_VALUES, REQUEST_SCOPE, SESSION_SCOPE, VIEW
+    };
+}
