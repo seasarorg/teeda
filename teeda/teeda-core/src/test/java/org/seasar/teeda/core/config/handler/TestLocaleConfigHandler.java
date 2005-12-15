3e9aaa72613496d08b94441044fca5d813445359
shot6 <shinpei.ohtani@gmail.com>
2005-12-15 16:00:28 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@63 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/config/handler/TestLocaleConfigHandler.java
@@ -0,0 +1,25 @@
+package org.seasar.teeda.core.config.handler;
+
+import org.seasar.teeda.core.unit.TeedaTestCase;
+
+public class TestLocaleConfigHandler extends TeedaTestCase {
+
+	public static void main(String[] args) {
+	}
+
+	public TestLocaleConfigHandler(String arg0) {
+		super(arg0);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testLocaleConfigHandler(){
+		notDoneYet();
+	}
+}
