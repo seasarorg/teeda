4c2c5f60cd22a6c61fcaef1c23725cbd3d87197b
shot6 <shinpei.ohtani@gmail.com>
2006-01-09 05:43:07 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@97 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/lifecycle/impl/ProcessValidationsPhase.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/lifecycle/impl/ProcessValidationsPhase.java
@@ -1,10 +1,18 @@
 package org.seasar.teeda.core.lifecycle.impl;
 
 import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
 
-public class ProcessValidationsPhase {
+import org.seasar.teeda.core.lifecycle.AbstractPhase;
+
+public class ProcessValidationsPhase extends AbstractPhase{
+	
 	public void executePhase(FacesContext context){
 		context.getViewRoot().processValidators(context);
+	}
+
+	protected PhaseId getCurrentPhaseId() {
+		return PhaseId.PROCESS_VALIDATIONS;
 	}
 
 }
