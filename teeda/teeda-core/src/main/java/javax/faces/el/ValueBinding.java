1b526b52d4b7d6b58e53f54b9478e1a26ac4008b
manhole <manholex@gmail.com>
2006-01-12 05:13:50 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@106 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/javax/faces/el/ValueBinding.java
+++ teeda/teeda-core/src/main/java/javax/faces/el/ValueBinding.java
@@ -22,22 +22,23 @@ import javax.faces.context.FacesContext;
  */
 public abstract class ValueBinding {
 
-	public ValueBinding() {
-	}
+    public ValueBinding() {
+    }
 
-	public abstract Object getValue(FacesContext facescontext)
-			throws EvaluationException, PropertyNotFoundException;
+    public abstract Object getValue(FacesContext context)
+        throws EvaluationException, PropertyNotFoundException;
 
-	public abstract void setValue(FacesContext facescontext, Object obj)
-			throws EvaluationException, PropertyNotFoundException;
+    public abstract void setValue(FacesContext context, Object value)
+        throws EvaluationException, PropertyNotFoundException;
 
-	public abstract boolean isReadOnly(FacesContext facescontext)
-			throws EvaluationException, PropertyNotFoundException;
+    public abstract boolean isReadOnly(FacesContext context)
+        throws EvaluationException, PropertyNotFoundException;
 
-	public abstract Class getType(FacesContext facescontext)
-			throws EvaluationException, PropertyNotFoundException;
+    public abstract Class getType(FacesContext context)
+        throws EvaluationException, PropertyNotFoundException;
 
-	public String getExpressionString() {
-		return null;
-	}
+    public String getExpressionString() {
+        return null;
+    }
+
 }
