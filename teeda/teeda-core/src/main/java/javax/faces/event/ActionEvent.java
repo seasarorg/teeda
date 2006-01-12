73459870b77ea715f6b05860d7762d2546f02edb
manhole <manholex@gmail.com>
2006-01-12 05:13:22 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@104 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/javax/faces/event/ActionEvent.java
+++ teeda/teeda-core/src/main/java/javax/faces/event/ActionEvent.java
@@ -2,21 +2,23 @@ package javax.faces.event;
 
 import javax.faces.component.UIComponent;
 
-
 public class ActionEvent extends FacesEvent {
 
-	public ActionEvent(UIComponent component){
-		super(component);
-		if(component == null){
-			throw new IllegalArgumentException("ActionEvent");
-		}
-	}
-	public boolean isAppropriateListener(FacesListener listener) {
-		return (listener instanceof ActionListener);
-	}
+    private static final long serialVersionUID = 1L;
 
-	public void processListener(FacesListener listener) {
-		((ActionListener)listener).processAction(this);
-	}
+    public ActionEvent(UIComponent component) {
+        super(component);
+        if (component == null) {
+            throw new IllegalArgumentException("ActionEvent");
+        }
+    }
+
+    public boolean isAppropriateListener(FacesListener listener) {
+        return (listener instanceof ActionListener);
+    }
+
+    public void processListener(FacesListener listener) {
+        ((ActionListener) listener).processAction(this);
+    }
 
 }
