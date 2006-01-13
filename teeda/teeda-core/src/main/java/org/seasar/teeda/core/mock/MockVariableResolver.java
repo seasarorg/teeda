ca54d71404dc9f799534f45aef4501f161b03a59
manhole <manholex@gmail.com>
2006-01-13 14:16:39 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@122 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/mock/MockVariableResolver.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/mock/MockVariableResolver.java
@@ -12,23 +12,37 @@ import org.seasar.teeda.core.JsfConstants;
 public class MockVariableResolver extends VariableResolver {
 
     private Map values_ = new HashMap();
+
     private boolean inited_ = false;
-    public MockVariableResolver(){
+
+    public MockVariableResolver() {
     }
-    
-    public void putValue(String key, Object value){
+
+    public void putValue(String key, Object value) {
         values_.put(key, value);
     }
-    
-    public Object resolveVariable(FacesContext context, String name) 
-    	throws EvaluationException {
-    	if(!inited_){
-    		values_.put(JsfConstants.APPLICATION_SCOPE, context.getExternalContext().getApplicationMap());
-    		values_.put(JsfConstants.SESSION_SCOPE, context.getExternalContext().getSessionMap());
-    		values_.put(JsfConstants.REQUEST_SCOPE, context.getExternalContext().getRequestMap());
-    		inited_ = true;
-    	}
+
+    public Object resolveVariable(FacesContext context, String name)
+            throws EvaluationException {
+        if (!inited_) {
+            mergePut(JsfConstants.APPLICATION_SCOPE, context.getExternalContext()
+                    .getApplicationMap());
+            mergePut(JsfConstants.SESSION_SCOPE, context.getExternalContext()
+                    .getSessionMap());
+            mergePut(JsfConstants.REQUEST_SCOPE, context.getExternalContext()
+                    .getRequestMap());
+            inited_ = true;
+        }
         return values_.get(name);
+    }
+
+    private void mergePut(String key, Map value) {
+        Map previous = (Map) values_.get(key);
+        if (previous != null) {
+            previous.putAll(value);
+        } else {
+            values_.put(key, value);
+        }
     }
 
 }
