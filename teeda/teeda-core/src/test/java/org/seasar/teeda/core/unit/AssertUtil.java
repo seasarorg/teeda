5ce4189b57f310f58c4601b7b4f9042afb907500
manhole <manholex@gmail.com>
2006-01-13 08:48:50 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@120 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/test/java/org/seasar/teeda/core/unit/AssertUtil.java
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/unit/AssertUtil.java
@@ -1,13 +1,24 @@
 package org.seasar.teeda.core.unit;
 
 import junit.framework.Assert;
+import junit.framework.AssertionFailedError;
 
 public class AssertUtil {
 
     public static void assertExceptionMessageExist(Throwable th) {
         String message = th.getMessage();
-        Assert.assertNotNull(message);
-        Assert.assertTrue(message.trim().length() > 0);
+        try {
+            Assert.assertNotNull("Throwable should have message", message);
+            Assert.assertTrue("Throwable should have message", message.trim()
+                    .length() > 0);
+        } catch (AssertionFailedError afe) {
+            th.printStackTrace();
+            throw afe;
+        }
+    }
+
+    public static void assertContains(String expected, String value) {
+        Assert.assertEquals(true, value.indexOf(expected) > -1);
     }
 
 }
