c43a06460d5900ea5ca1c877fbc51742d88a8dc6
manhole <manholex@gmail.com>
2006-01-14 18:42:37 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@138 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/javax/faces/component/UIParameter.java
+++ teeda/teeda-core/src/main/java/javax/faces/component/UIParameter.java
@@ -1,8 +1,6 @@
 package javax.faces.component;
 
 import javax.faces.context.FacesContext;
-import javax.faces.el.ValueBinding;
-
 
 public class UIParameter extends UIComponentBase {
 
@@ -11,55 +9,56 @@ public class UIParameter extends UIComponentBase {
     public static final String COMPONENT_FAMILY = "javax.faces.Parameter";
 
     private String name_ = null;
-    
+
     private Object value_ = null;
-    
+
     private static final String NAME_BINDING_NAME = "name";
 
     private static final String VALUE_BINDING_NAME = "value";
-    
-    public UIParameter(){
-    	super();
-    }
-    
-	public String getFamily() {
-		return COMPONENT_FAMILY;
-	}
-	
-	public String getName(){
-		if(name_ != null){
-			return name_;
-		}
-		return (String)ComponentUtils_.getValueBindingValue(this, NAME_BINDING_NAME);
-	}
 
-	public void setName(String name){
-		name_ = name;
-	}
-	
-	public Object getValue(){
-		if(value_ != null){
-			return value_;
-		}
-		return ComponentUtils_.getValueBindingValue(this, VALUE_BINDING_NAME);
-	}
-	
-	public void setValue(Object value){
-		value_ = value;
-	}
-	
-	public void restoreState(FacesContext context, Object state) {
-		Object[] values = (Object[])state;
-		super.restoreState(context, values[0]);
-		name_ = (String)values[1];
-		value_ = values[2];
-	}
-	
-	public Object saveState(FacesContext context) {
-		Object[] values = new Object[3];
-		values[0] = super.saveState(context);
-		values[1] = name_;
-		values[2] = value_;
-		return values;
-	}
+    public UIParameter() {
+        super();
+    }
+
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
+
+    public String getName() {
+        if (name_ != null) {
+            return name_;
+        }
+        return (String) ComponentUtils_.getValueBindingValue(this,
+                NAME_BINDING_NAME);
+    }
+
+    public void setName(String name) {
+        name_ = name;
+    }
+
+    public Object getValue() {
+        if (value_ != null) {
+            return value_;
+        }
+        return ComponentUtils_.getValueBindingValue(this, VALUE_BINDING_NAME);
+    }
+
+    public void setValue(Object value) {
+        value_ = value;
+    }
+
+    public void restoreState(FacesContext context, Object state) {
+        Object[] values = (Object[]) state;
+        super.restoreState(context, values[0]);
+        name_ = (String) values[1];
+        value_ = values[2];
+    }
+
+    public Object saveState(FacesContext context) {
+        Object[] values = new Object[3];
+        values[0] = super.saveState(context);
+        values[1] = name_;
+        values[2] = value_;
+        return values;
+    }
 }
