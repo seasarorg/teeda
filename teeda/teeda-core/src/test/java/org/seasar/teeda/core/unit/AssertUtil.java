1c0d36f3fe428bf49129610a90feaed9288c1e12
manhole <manholex@gmail.com>
2006-01-14 06:18:40 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@133 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/test/java/org/seasar/teeda/core/unit/AssertUtil.java
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/unit/AssertUtil.java
@@ -17,8 +17,20 @@ public class AssertUtil {
         }
     }
 
-    public static void assertContains(String expected, String value) {
-        Assert.assertEquals(true, value.indexOf(expected) > -1);
+    public static void assertContains(String shouldBeContained, String value) {
+        Assert.assertEquals(true, value.indexOf(shouldBeContained) > -1);
+    }
+
+    public static void assertNotEquals(Object o1, Object o2) {
+        AssertionFailedError afe = null;
+        try {
+            Assert.assertEquals(o1, o2);
+        } catch (AssertionFailedError expected) {
+            afe = expected;
+        }
+        if (afe == null) {
+            Assert.fail("expected not equals");
+        }
     }
 
 }
