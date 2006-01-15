a1e523962132d31119d07f9bf7c718ff9d260529
shot6 <shinpei.ohtani@gmail.com>
2006-01-15 13:56:17 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@145 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/config/element/impl/NavigationCaseElementImpl.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/config/element/impl/NavigationCaseElementImpl.java
@@ -5,13 +5,17 @@ import org.seasar.teeda.core.config.element.NavigationCaseElement;
 /**
  * @author Shinpei Ohtani(aka shot)
  */
-public class NavigationCaseElementImpl implements NavigationCaseElement{
+public class NavigationCaseElementImpl implements NavigationCaseElement {
 
     private String fromAction_;
+
     private String fromOutcome_;
+
     private String toViewId_ = DEFAULT_TO_VIEW_ID;
+
     private boolean redirect_ = false;
-    public NavigationCaseElementImpl(){
+
+    public NavigationCaseElementImpl() {
     }
 
     public void setFromAction(String fromAction) {
@@ -23,6 +27,9 @@ public class NavigationCaseElementImpl implements NavigationCaseElement{
     }
 
     public void setToViewId(String toViewId) {
+        if (toViewId == null) {
+            throw new IllegalArgumentException("toViewID");
+        }
         toViewId_ = toViewId;
     }
 
@@ -45,5 +52,20 @@ public class NavigationCaseElementImpl implements NavigationCaseElement{
     public boolean isRedirect() {
         return redirect_;
     }
-    
+
+    public boolean equals(Object obj) {
+        if (!(obj instanceof NavigationCaseElementImpl)) {
+            return false;
+        }
+        NavigationCaseElementImpl navCase = (NavigationCaseElementImpl) obj;
+        String fromAction = navCase.getFromAction();
+        String fromOutcome = navCase.getFromOutcome();
+        String toViewId = navCase.getToViewId();
+        return (fromAction == null ? fromAction_ == null : fromAction
+                .equals(fromAction_))
+                && (fromOutcome == null ? fromOutcome_ == null : fromOutcome
+                        .equals(fromOutcome_))
+                && (toViewId == null ? toViewId_ == null : toViewId
+                        .equals(toViewId));
+    }
 }
