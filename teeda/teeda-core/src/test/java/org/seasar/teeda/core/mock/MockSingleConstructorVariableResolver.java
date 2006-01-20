9836a69853768f43cc2b7d6140606c8b82acb0fe
shot6 <shinpei.ohtani@gmail.com>
2006-01-20 02:54:58 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@199 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/mock/MockSingleConstructorVariableResolver.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.mock;
+
+import javax.faces.context.FacesContext;
+import javax.faces.el.EvaluationException;
+import javax.faces.el.VariableResolver;
+
+/**
+ * @author shot
+ */
+public class MockSingleConstructorVariableResolver extends VariableResolver {
+
+    private VariableResolver resolver_;
+    
+    public MockSingleConstructorVariableResolver(VariableResolver resolver) {
+        resolver_ = resolver;
+    }
+    
+    public Object resolveVariable(FacesContext context, String name)
+            throws EvaluationException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public VariableResolver getOriginal() {
+        return resolver_;
+    }
+}
