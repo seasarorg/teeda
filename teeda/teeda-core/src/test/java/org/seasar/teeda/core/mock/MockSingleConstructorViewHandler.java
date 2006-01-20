808151ab4373690d89bbd43cc4160f641a1a0a75
shot6 <shinpei.ohtani@gmail.com>
2006-01-20 03:09:25 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@200 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/mock/MockSingleConstructorViewHandler.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.mock;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.faces.FacesException;
+import javax.faces.application.ViewHandler;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+/**
+ * @author shot
+ */
+public class MockSingleConstructorViewHandler extends ViewHandler {
+
+    private ViewHandler originalHandler_;
+    public MockSingleConstructorViewHandler(ViewHandler originalHandler) {
+        originalHandler_ = originalHandler;
+    }
+    
+    public Locale calculateLocale(FacesContext context) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String calculateRenderKitId(FacesContext context) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public UIViewRoot createView(FacesContext context, String viewId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getActionURL(FacesContext context, String viewId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getResourceURL(FacesContext context, String path) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void renderView(FacesContext context, UIViewRoot viewToRender)
+            throws IOException, FacesException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public UIViewRoot restoreView(FacesContext context, String viewId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void writeState(FacesContext context) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public ViewHandler getOriginal() {
+        return originalHandler_;
+    }
+}
