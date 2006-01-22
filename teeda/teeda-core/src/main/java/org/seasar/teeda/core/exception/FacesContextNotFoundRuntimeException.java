21892700b102befafaef756afffdc760cc2e801e
shot6 <shinpei.ohtani@gmail.com>
2006-01-22 09:44:17 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@231 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/exception/FacesContextNotFoundRuntimeException.java
@@ -0,0 +1,8 @@
+package org.seasar.teeda.core.exception;
+
+public class FacesContextNotFoundRuntimeException extends ExtendFacesException {
+
+    public FacesContextNotFoundRuntimeException(){
+        super("ETDA0017");
+    }
+}
