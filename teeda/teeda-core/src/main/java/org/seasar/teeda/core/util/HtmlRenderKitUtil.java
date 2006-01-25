a46a540a6955fabe313d887f56834560168ac313
shot6 <shinpei.ohtani@gmail.com>
2006-01-25 16:49:26 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@255 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/util/HtmlRenderKitUtil.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.util;
+
+import javax.faces.context.FacesContext;
+
+import org.seasar.framework.util.StringUtil;
+import org.seasar.teeda.core.JsfConstants;
+
+/**
+ * @author shot
+ */
+public class HtmlRenderKitUtil {
+
+    public static String[] removeSemiColon(String[] contentTypes) {
+        for (int i = 0; i < contentTypes.length; i++) {
+            String type = contentTypes[i];
+            int index = type.indexOf(";");
+            if (index != -1) {
+                type = type.substring(0, index);
+                contentTypes[i] = type;
+            }
+        }
+        return contentTypes;
+    }
+
+    public static String getContentType(String contentTypeList) {
+        if (contentTypeList == null) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            contentTypeList = getContentType(context);
+            if (contentTypeList == null) {
+                return JsfConstants.HTML_CONTENT_TYPE;
+            }
+        }
+
+        String[] strs = StringUtil.split(contentTypeList, ",");
+        String[] contentTypes = removeSemiColon(strs);
+
+        for (int i = 0; i < contentTypes.length; i++) {
+            String type = contentTypes[i];
+            if (isHtmlContentType(type)) {
+                return JsfConstants.HTML_CONTENT_TYPE;
+            } else if (isXmlContentType(type)) {
+                return JsfConstants.XHTML_CONTENT_TYPE;
+            }
+        }
+        return null;
+    }
+
+    public static String getContentType(FacesContext context) {
+        return (String) context.getExternalContext().getRequestHeaderMap().get(
+                "Accept");
+
+    }
+
+    public static boolean isHtmlContentType(String type) {
+        return type.indexOf("html") != -1
+                || type.equals(JsfConstants.ANY_CONTENT_TYPE);
+    }
+
+    public static boolean isXmlContentType(String type) {
+        return type.indexOf("xml") != -1;
+    }
+}
---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/util/HtmlRenderKitUtilTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2004-2005 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.util;
+
+import org.seasar.teeda.core.unit.TeedaTestCase;
+
+/**
+ * @author shot
+ */
+public class HtmlRenderKitUtilTest extends TeedaTestCase{
+
+    public void testRemoveSemiColon_singleValues() throws Exception {
+        String[] strs = HtmlRenderKitUtil.removeSemiColon(new String[]{"aaa;"});
+        assertEquals("aaa", strs[0]);
+    }
+
+    public void testRemoveSemiColon_multipleValues() throws Exception {
+        String[] strs = HtmlRenderKitUtil.removeSemiColon(new String[]{"aaa", "bbb;"});
+        assertEquals("aaa", strs[0]);
+        assertEquals("bbb", strs[1]);
+    }
+    
+    public void testGetContentTypeFromFacesContext() throws Exception {
+        getExternalContext().getRequestMap().put("Accept", "hoge");
+        assertEquals("hoge", getExternalContext().getRequestMap().get("Accept"));
+    }
+    
+    public void testIsHtmlContentType() throws Exception {
+        assertFalse(HtmlRenderKitUtil.isHtmlContentType("hoge"));
+        assertTrue(HtmlRenderKitUtil.isHtmlContentType("text/html"));
+        assertTrue(HtmlRenderKitUtil.isHtmlContentType("*/*"));
+        assertTrue(HtmlRenderKitUtil.isHtmlContentType("html/foo"));
+    }
+
+    public void testIsXmlContentType() throws Exception {
+        assertFalse(HtmlRenderKitUtil.isXmlContentType("hoge"));
+        assertTrue(HtmlRenderKitUtil.isXmlContentType("application/xhtml+xml"));
+        assertTrue(HtmlRenderKitUtil.isXmlContentType("application/xml"));
+        assertTrue(HtmlRenderKitUtil.isXmlContentType("text/xml"));
+    }
+
+}
