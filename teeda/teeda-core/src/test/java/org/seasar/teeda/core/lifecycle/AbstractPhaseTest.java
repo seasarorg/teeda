1952173e69e2b8a3ca7c54c93038d6b821714823
shot6 <shinpei.ohtani@gmail.com>
2006-02-05 02:22:34 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@337 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/lifecycle/AbstractPhaseTest.java
@@ -0,0 +1,81 @@
+package org.seasar.teeda.core.lifecycle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import javax.faces.lifecycle.Lifecycle;
+
+import org.seasar.teeda.core.mock.MockLifecycle;
+import org.seasar.teeda.core.unit.TeedaTestCase;
+
+public class AbstractPhaseTest extends TeedaTestCase {
+
+    private PhaseListener[] listeners_;
+    protected void setUp() throws Exception {
+        listeners_ = getLifecycle().clearAllPhaseListener();
+    }
+
+    protected void tearDown() throws Exception {
+        getLifecycle().setupDefaultPhaseListener(listeners_);
+    }
+
+    public void testPrePhase() throws Exception {
+        // # Arrange #
+        List list = new ArrayList();
+        MockBeforePhaseNotifyListener listener = new MockBeforePhaseNotifyListener(
+                list);
+        getLifecycle().addPhaseListener(listener);
+        TargetPhase phase = new TargetPhase();
+        phase.setPhaseId(PhaseId.APPLY_REQUEST_VALUES);
+        
+        // # Act #
+        phase.prePhase(getFacesContext());
+
+        // # Assert #
+        assertEquals(1, list.size());
+        PhaseEvent event = (PhaseEvent) list.get(0);
+        assertEquals(PhaseId.APPLY_REQUEST_VALUES, event.getPhaseId());
+        assertEquals(getLifecycle(), event.getSource());
+    }
+
+    private static class TargetPhase extends AbstractPhase {
+
+        private PhaseId phaseId_ = PhaseId.ANY_PHASE;
+        
+        protected void executePhase(FacesContext context) {
+        }
+
+        protected PhaseId getCurrentPhaseId() {
+            return phaseId_;
+        }
+
+        protected void setPhaseId(PhaseId phaseId) {
+            phaseId_ = phaseId;
+        }
+    }
+
+    private static class MockBeforePhaseNotifyListener implements PhaseListener {
+
+        private List notify_;
+
+        public MockBeforePhaseNotifyListener(List notify) {
+            notify_ = notify;
+        }
+
+        public void afterPhase(PhaseEvent event) {
+        }
+
+        public void beforePhase(PhaseEvent event) {
+            notify_.add(event);
+        }
+
+        public PhaseId getPhaseId() {
+            return PhaseId.APPLY_REQUEST_VALUES;
+        }
+
+    }
+}
