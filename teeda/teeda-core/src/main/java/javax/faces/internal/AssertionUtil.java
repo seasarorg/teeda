60782bad5d37e4077aeb328042e0aa0656224caa
shot6 <shinpei.ohtani@gmail.com>
2006-02-05 10:38:18 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@340 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/main/java/javax/faces/internal/AssertionUtil.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package javax.faces.internal;
+
+/**
+ * @author shot
+ */
+public class AssertionUtil {
+
+    private AssertionUtil(){
+    }
+    
+    public static void assertNotNull(Object obj) {
+        assertNotNull(null, obj);
+    }
+
+    public static void assertNotNull(String message, Object obj) {
+        if (obj == null) {
+            throw new NullPointerException(message);
+        }
+    }
+
+}
