d3bf4b6d73bb37d189203123faab572769a3379f
shot6 <shinpei.ohtani@gmail.com>
2006-02-17 01:55:04 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@414 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/application/TeedaStateManager.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.application;
+
+import javax.faces.application.StateManager;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+import org.seasar.teeda.core.application.impl.TeedaStateManagerImplTest;
+
+/**
+ * @author shot
+ */
+public abstract class TeedaStateManager extends StateManager {
+    
+    protected static final String SERIALIZED_VIEW_ATTR = TeedaStateManagerImplTest.class
+            .getName()
+            + ".SERIALIZED_VIEW";
+
+    private TreeStructureManager treeStructureManager_;
+    
+    protected abstract void restoreComponentStateFromClient(
+            FacesContext context, UIViewRoot viewRoot, String renderKitId);
+
+    protected abstract void restoreComponentStateFromServer(
+            FacesContext context, UIViewRoot viewRoot);
+
+    protected abstract UIViewRoot restoreTreeStructureFromClient(
+            FacesContext context, String viewId, String renderKitId);
+
+    protected abstract UIViewRoot restoreTreeStructureFromServer(
+            FacesContext context, String viewId);
+
+    public TreeStructureManager getTreeStructureManager() {
+        return treeStructureManager_;
+    }
+    
+    public void setTreeStructureManager(TreeStructureManager treeStructureManager) {
+        treeStructureManager_ = treeStructureManager;
+    }
+}
