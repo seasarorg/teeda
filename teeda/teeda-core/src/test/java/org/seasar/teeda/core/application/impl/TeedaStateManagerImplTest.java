b70cee23448637b4ff9e112859514db084f06eec
shot6 <shinpei.ohtani@gmail.com>
2006-02-17 01:54:38 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@412 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/application/impl/TeedaStateManagerImplTest.java
@@ -0,0 +1,8 @@
+package org.seasar.teeda.core.application.impl;
+
+import org.seasar.teeda.core.unit.TeedaTestCase;
+
+public class TeedaStateManagerImplTest extends TeedaTestCase {
+
+    //TODO test save and restore view
+}
---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/application/impl/TreeStructureManagerImplTest.java
@@ -0,0 +1,8 @@
+package org.seasar.teeda.core.application.impl;
+
+import junit.framework.TestCase;
+
+public class TreeStructureManagerImplTest extends TestCase {
+
+    //TODO testing
+}
