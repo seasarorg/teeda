68bf9664780e484b4cb18e0790954b781198471e
shot6 <shinpei.ohtani@gmail.com>
2006-02-18 13:07:21 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@434 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/test/java/org/seasar/teeda/core/application/impl/TreeStructureManagerImplTest.java
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/application/impl/TreeStructureManagerImplTest.java
@@ -1,5 +1,7 @@
 package org.seasar.teeda.core.application.impl;
 
+import javax.faces.component.UIComponent;
+
 import junit.framework.TestCase;
 
 import org.seasar.teeda.core.application.TreeStructure;
@@ -7,7 +9,6 @@ import org.seasar.teeda.core.mock.MockUIComponent;
 
 public class TreeStructureManagerImplTest extends TestCase {
 
-    //TODO testing
     public void testBuildTreeStructure_simple() throws Exception {
         // # Arrange #
         TreeStructureManagerImpl manager = new TreeStructureManagerImpl();
@@ -21,7 +22,36 @@ public class TreeStructureManagerImplTest extends TestCase {
         assertEquals(component.getClass().getName(), struct.getComponentClassName());
         assertEquals("id", struct.getComponentId());
     }
-    
+
+    public void testBuildTreeStructure_hasChildrenAndFacets() throws Exception {
+        // # Arrange #
+        TreeStructureManagerImpl manager = new TreeStructureManagerImpl();
+        MockUIComponent parent = new MockUIComponent();
+        parent.setId("parent");
+        MockUIComponent child1 = new MockUIComponent();
+        child1.setId("child1");
+        parent.getChildren().add(child1);
+        MockUIComponent child2 = new MockUIComponent();
+        child2.setId("child2");
+        parent.getFacets().put("hoge", child2);
+
+        // # Act #
+        TreeStructure struct = manager.buildTreeStructure(parent);
+        
+        // # Assert #
+        assertEquals(parent.getClass().getName(), struct.getComponentClassName());
+        assertEquals("parent", struct.getComponentId());
+        TreeStructure[] children = struct.getChildren();
+        assertEquals(1, children.length);
+        TreeStructure childStruct = children[0];
+        assertEquals("child1", childStruct.getComponentId());
+        Object[] facets = struct.getFacets();
+        assertEquals(1, facets.length);
+        Object[] childFacets = (Object[]) facets[0];
+        assertEquals("hoge", childFacets[0]);
+        assertEquals("child2", ((TreeStructure)childFacets[1]).getComponentId());
+    }
+
     public void testBuildChildrenTreeStructure_simple() throws Exception {
         // # Arrange #
         TreeStructureManagerImpl manager = new TreeStructureManagerImpl();
@@ -40,6 +70,19 @@ public class TreeStructureManagerImplTest extends TestCase {
         TreeStructure struct = structs[0];
         assertEquals(child.getClass().getName(), struct.getComponentClassName());
         assertEquals("child", struct.getComponentId());
+    }
+
+    public void testBuildChildrenTreeStructure_null() throws Exception {
+        // # Arrange #
+        TreeStructureManagerImpl manager = new TreeStructureManagerImpl();
+        MockUIComponent parent = new MockUIComponent();
+        parent.setId("parent");
+
+        // # Act #
+        TreeStructure[] structs = manager.buildChildrenTreeStructure(parent);
+
+        // # Assert #
+        assertNull(structs);
     }
     
     public void testBuildFacetsTreeStructure_simple() throws Exception {
@@ -63,4 +106,65 @@ public class TreeStructureManagerImplTest extends TestCase {
         assertEquals(child.getClass().getName(), struct.getComponentClassName());
         assertEquals("child", struct.getComponentId());
     }
+    
+    public void testBuildFacetsTreeStructure_null() throws Exception {
+        // # Arrange #
+        TreeStructureManagerImpl manager = new TreeStructureManagerImpl();
+        MockUIComponent parent = new MockUIComponent();
+        parent.setId("parent");
+
+        // # Act #
+        Object[] array = manager.buildFacetsTreeStructure(parent);
+
+        // # Assert #
+        assertNull(array);
+    }
+
+    public void testRestoreTreeStructure_simple() throws Exception {
+        // # Arrange #
+        TreeStructureManagerImpl manager = new TreeStructureManagerImpl();
+        TreeStructure struct = new TreeStructure(MockUIComponent.class.getName(), "id");
+
+        // # Act #
+        UIComponent component = manager.restoreTreeStructure(struct);
+        
+        // # Assert #
+        assertNotNull(component);
+        assertTrue(component instanceof MockUIComponent);
+        assertEquals("id", component.getId());
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
