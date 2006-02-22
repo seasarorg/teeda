7976b60adb784b04347864227f32f5dd3cbb1b2c
manhole <manholex@gmail.com>
2006-02-22 02:42:09 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@480 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/test/java/org/seasar/teeda/core/spike/DOMImplementationTest.java
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/spike/DOMImplementationTest.java
@@ -9,38 +9,25 @@ import java.util.Map.Entry;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
-import junitx.framework.StringAssert;
-
-import org.apache.commons.lang.SystemUtils;
 
 public class DOMImplementationTest extends TestCase {
 
     public void testToKnowDOMImplementation() throws Exception {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        System.out.println(factory);
+        assertNotNull(factory);
+
         DocumentBuilder builder = factory.newDocumentBuilder();
-        final String factoryName = factory.getClass().getName();
-        final String builderName = builder.getClass().getName();
-        try {
-            if (SystemUtils.isJavaVersionAtLeast(1.5F)) {
-                StringAssert.assertStartsWith("com.sun.org.apache.xerces",
-                        factoryName);
-                StringAssert.assertStartsWith("com.sun.org.apache.xerces",
-                        builderName);
-            } else {
-                StringAssert.assertStartsWith("org.apache.xerces", factoryName);
-                StringAssert.assertStartsWith("org.apache.xerces", builderName);
-            }
-        } catch (AssertionFailedError e) {
-            Properties properties = System.getProperties();
-            for (Iterator it = new TreeMap(properties).entrySet().iterator(); it
-                    .hasNext();) {
-                Map.Entry entry = (Entry) it.next();
-                System.out.println(entry.getKey() + "=[" + entry.getValue()
-                        + "]");
-            }
-            throw e;
+        System.out.println(builder);
+        assertNotNull(builder);
+
+        Properties properties = System.getProperties();
+        for (Iterator it = new TreeMap(properties).entrySet().iterator(); it
+                .hasNext();) {
+            Map.Entry entry = (Entry) it.next();
+            System.out.println(entry.getKey() + "=[" + entry.getValue() + "]");
         }
     }
+
 }
