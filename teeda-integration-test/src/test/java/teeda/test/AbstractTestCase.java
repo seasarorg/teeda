7e02bb8bf0afb24c8baa5c18064f8e526550dd3d
manhole <manholex@gmail.com>
2006-02-27 03:25:22 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@535 e0972215-f904-0410-97d3-99f68b247671

--- teeda-integration-test/src/test/java/teeda/test/AbstractTestCase.java
+++ teeda-integration-test/src/test/java/teeda/test/AbstractTestCase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package teeda.test;
 
 import java.io.File;
@@ -14,6 +29,9 @@ import org.seasar.jsf.selenium.WebApplicationTestSetup;
 import com.gargoylesoftware.htmlunit.WebResponse;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+/**
+ * @author manhole
+ */
 public abstract class AbstractTestCase extends TestCase {
 
     protected static WebApplicationTestSetup setUpTestSuite(
--- teeda-integration-test/src/test/java/teeda/test/HelloWorldTest.java
+++ teeda-integration-test/src/test/java/teeda/test/HelloWorldTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package teeda.test;
 
 import java.net.URL;
@@ -8,6 +23,9 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSpan;
 
+/**
+ * @author manhole
+ */
 public class HelloWorldTest extends AbstractTestCase {
 
     public static Test suite() throws Exception {
