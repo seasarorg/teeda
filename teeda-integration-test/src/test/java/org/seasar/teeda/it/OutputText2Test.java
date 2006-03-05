4d0590539e1a36f00dba005a1a4249f9d0646e8a
shot6 <shinpei.ohtani@gmail.com>
2006-03-05 03:21:19 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@592 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/src/test/java/org/seasar/teeda/it/OutputText2Test.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it;
+
+import java.net.URL;
+
+import junit.framework.Test;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSpan;
+
+/**
+ * @author manhole
+ */
+public class OutputText2Test extends AbstractTestCase {
+
+    public static Test suite() throws Exception {
+        return setUpTestSuite(OutputText2Test.class);
+    }
+
+    public void testOutputLink() throws Exception {
+        // ## Arrange ##
+        URL url = getUrl("faces/outputText2.jsp");
+        System.out.println(url);
+
+        WebClient webClient = new WebClient();
+
+        // ## Act ##
+        HtmlPage page = (HtmlPage) webClient.getPage(url);
+
+        // ## Assert ##
+        assertEquals("this is outputText2.jsp", page.getTitleText());
+        final String body = getBody(page).trim();
+        System.out.println(body);
+
+        HtmlSpan span = (HtmlSpan) page.getHtmlElementById("hello");
+        assertEquals("Hello OutputText2", span.asText());
+    }
+
+}
