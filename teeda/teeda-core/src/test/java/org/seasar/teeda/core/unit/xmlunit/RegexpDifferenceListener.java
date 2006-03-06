05f6212a7197f13099ae9af26652cd74e129623c
manhole <manholex@gmail.com>
2006-03-06 16:52:36 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@642 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/unit/xmlunit/RegexpDifferenceListener.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.unit.xmlunit;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.custommonkey.xmlunit.Difference;
+import org.custommonkey.xmlunit.DifferenceConstants;
+import org.custommonkey.xmlunit.DifferenceListener;
+import org.custommonkey.xmlunit.NodeDetail;
+import org.w3c.dom.Node;
+
+/**
+ * @author manhole
+ */
+public class RegexpDifferenceListener implements DifferenceListener {
+
+    public int differenceFound(Difference difference) {
+        if (DifferenceConstants.TEXT_VALUE.getId() == difference.getId()
+                || DifferenceConstants.ATTR_VALUE.getId() == difference.getId()) {
+            // 14, 3
+            NodeDetail cNodeDetail = difference.getControlNodeDetail();
+            NodeDetail tNodeDetail = difference.getTestNodeDetail();
+            String cNodeDetailValue = cNodeDetail.getValue();
+            String tNodeDetailValue = tNodeDetail.getValue();
+
+            Pattern pattern = Pattern.compile(cNodeDetailValue);
+            Matcher matcher = pattern.matcher(tNodeDetailValue);
+            if (matcher.matches()) {
+                System.out.println("recover:TEXT_VALUE || ATTR_VALUE (Regexp)");
+                return DifferenceListener.RETURN_IGNORE_DIFFERENCE_NODES_SIMILAR;
+            }
+        }
+        return DifferenceListener.RETURN_ACCEPT_DIFFERENCE;
+    }
+
+    public void skippedComparison(Node node1, Node node2) {
+    }
+
+}
---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/unit/xmlunit/RegexpDifferenceListenerTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.unit.xmlunit;
+
+import junit.framework.TestCase;
+
+import org.custommonkey.xmlunit.Diff;
+
+/**
+ * @author manhole
+ */
+public class RegexpDifferenceListenerTest extends TestCase {
+
+    public void testRegexp1() throws Exception {
+        Diff diff = new Diff("<a>1..</a>", "<a>123</a>");
+        diff.overrideDifferenceListener(new RegexpDifferenceListener());
+        assertEquals(diff.toString(), true, diff.similar());
+        assertEquals(diff.toString(), false, diff.identical());
+    }
+
+    public void testRegexp2() throws Exception {
+        Diff diff = new Diff("<a>1..</a>", "<a>1234</a>");
+        diff.overrideDifferenceListener(new RegexpDifferenceListener());
+        assertEquals(diff.toString(), false, diff.similar());
+        assertEquals(diff.toString(), false, diff.identical());
+    }
+
+    public void testRegexp3() throws Exception {
+        Diff diff = new Diff("<a href=\"a.jsp.*\">a</a>",
+                "<a href=\"a.jsp?1234=5555\">a</a>");
+        diff.overrideDifferenceListener(new RegexpDifferenceListener());
+        assertEquals(diff.toString(), true, diff.similar());
+    }
+
+}
