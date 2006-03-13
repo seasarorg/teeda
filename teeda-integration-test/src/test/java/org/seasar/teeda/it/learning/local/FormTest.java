ce4ab37a0d4b4b0bab89f293a0dc65fb15232f96
manhole <manholex@gmail.com>
2006-03-13 01:38:05 +0000

    HtmlUnit example: using xpath to identify node
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@720 e0972215-f904-0410-97d3-99f68b247671

--- teeda-integration-test/src/test/java/org/seasar/teeda/it/learning/local/FormTest.java
+++ teeda-integration-test/src/test/java/org/seasar/teeda/it/learning/local/FormTest.java
@@ -2,12 +2,14 @@ package org.seasar.teeda.it.learning.local;
 
 import java.net.URL;
 
+import org.jaxen.XPath;
 import org.seasar.teeda.it.AbstractTestCase;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.xpath.HtmlUnitXPath;
 
 public class FormTest extends AbstractTestCase {
 
@@ -21,8 +23,9 @@ public class FormTest extends AbstractTestCase {
         // ## Act ##
         HtmlPage page1 = (HtmlPage) webClient.getPage(url);
         HtmlForm form = page1.getFormByName("myFormName");
+
         HtmlSubmitInput button = (HtmlSubmitInput) form
-            .getInputByName("doSubmitName");
+                .getInputByName("doSubmitName");
         HtmlPage page2 = (HtmlPage) button.click();
 
         // ## Assert ##
@@ -40,8 +43,31 @@ public class FormTest extends AbstractTestCase {
         // ## Act ##
         HtmlPage page1 = (HtmlPage) webClient.getPage(url);
         HtmlForm form = (HtmlForm) page1.getHtmlElementById("myFormId");
+
         HtmlSubmitInput button = (HtmlSubmitInput) form
-            .getHtmlElementById("doSubmitId");
+                .getHtmlElementById("doSubmitId");
+        HtmlPage page2 = (HtmlPage) button.click();
+
+        // ## Assert ##
+        assertEquals("end", page2.getTitleText());
+        assertEquals(getFileAsUrl("end.html"), page2.getWebResponse().getUrl());
+    }
+
+    public void testFormSubmit_UsingXPath() throws Exception {
+        // ## Arrange ##
+        URL url = getFileAsUrl("formSubmit1.html");
+        System.out.println(url);
+
+        WebClient webClient = new WebClient();
+
+        // ## Act ##
+        HtmlPage page1 = (HtmlPage) webClient.getPage(url);
+
+        XPath path = new HtmlUnitXPath("//form[@id='myFormId']");
+        HtmlForm form = (HtmlForm) path.selectSingleNode(page1);
+
+        HtmlSubmitInput button = (HtmlSubmitInput) form
+                .getHtmlElementById("doSubmitId");
         HtmlPage page2 = (HtmlPage) button.click();
 
         // ## Assert ##
