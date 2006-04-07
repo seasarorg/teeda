c65d2829b72c1b8d609eb8d8cb4642e42d4f59b0
manhole <manholex@gmail.com>
2006-04-07 07:11:59 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@792 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/src/test/java/org/seasar/teeda/it/converter/DateTimeTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it.converter;
+
+import java.net.URL;
+
+import junit.framework.Test;
+
+import org.seasar.teeda.it.AbstractTestCase;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSpan;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+
+/**
+ * @author manhole
+ */
+public class DateTimeTest extends AbstractTestCase {
+
+    public static Test suite() throws Exception {
+        return setUpTestSuite(DateTimeTest.class);
+    }
+
+    public void testRender() throws Exception {
+        // ## Arrange ##
+        URL url = getUrl("faces/converter/dateTime.jsp");
+        System.out.println(url);
+
+        WebClient webClient = new WebClient();
+
+        // ## Act ##
+        // ## Assert ##
+        HtmlPage page1 = (HtmlPage) webClient.getPage(url);
+
+        final String body1 = getBody(page1).trim();
+        System.out.println(body1);
+        assertEquals("dateTime.jsp", page1.getTitleText());
+
+        HtmlTextInput aaa1 = getAaa(page1);
+        assertEquals("", aaa1.getValueAttribute());
+        aaa1.setValueAttribute("2011.12.23");
+
+        HtmlSubmitInput submit1 = (HtmlSubmitInput) page1
+                .getHtmlElementById("submit1");
+
+        // 2
+
+        HtmlPage page2 = (HtmlPage) submit1.click();
+        final String body2 = getBody(page2);
+        System.out.println(body2.trim());
+
+        HtmlTextInput aaa2 = getAaa(page2);
+        assertEquals("2011.12.23", aaa2.getValueAttribute());
+        HtmlSpan bbb2 = getBbb(page2);
+        assertEquals("2011/12/23", bbb2.asText());
+    }
+
+    private HtmlSpan getBbb(HtmlPage page2) {
+        return (HtmlSpan) page2.getHtmlElementById("bbb");
+    }
+
+    private HtmlTextInput getAaa(HtmlPage page1) {
+        return (HtmlTextInput) page1.getHtmlElementById("aaa");
+    }
+
+}
