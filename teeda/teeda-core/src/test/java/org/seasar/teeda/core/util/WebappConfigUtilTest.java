da111af5b2108816f6fdf97b0e082b065c8b9eb6
shot6 <shinpei.ohtani@gmail.com>
2006-04-28 04:02:18 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@858 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/test/java/org/seasar/teeda/core/util/WebappConfigUtilTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.util;
+
+import org.seasar.teeda.core.config.webapp.element.WebappConfig;
+import org.seasar.teeda.core.config.webapp.element.impl.WebappConfigImpl;
+import org.seasar.teeda.core.unit.TeedaTestCase;
+
+/**
+ * @author shot
+ */
+public class WebappConfigUtilTest extends TeedaTestCase {
+
+    public void testGetWebappConfig() throws Exception {
+        WebappConfig config = new WebappConfigImpl();
+        getExternalContext().getApplicationMap().put(
+                WebappConfig.class.getName(), config);
+
+        WebappConfig c = WebappConfigUtil.getWebappConfig(getFacesContext());
+        assertEquals(config, c);
+    }
+}
