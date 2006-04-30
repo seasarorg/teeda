f2d89c1bdd78250049ab798bc0f0d47e7246421d
shot6 <shinpei.ohtani@gmail.com>
2006-04-30 15:36:01 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@874 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/annotation/AnnotationConstants.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.annotation;
+
+/**
+ * @author shot
+ * 
+ */
+public interface AnnotationConstants {
+
+    String EXPORT_SUFFIX = "_EXPORT";
+
+    String ACTION_BINDING_SUFFIX = "_ACTION_BINDING";
+
+    String VALUE = "value";
+
+    String BINDING_TYPE = "bindingType";
+
+    String NONE = "none";
+
+    String SESSION = "session";
+
+    String VALIDATOR = "_VALIDATOR";
+    
+    String ALIAS = "ALIAS";
+}
