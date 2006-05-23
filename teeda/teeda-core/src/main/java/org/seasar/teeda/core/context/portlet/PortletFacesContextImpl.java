0dc1b055128d5c652a3f4ebfd62fdb82a3b60aa9
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2006-05-23 12:15:00 +0000

    Copied from ServletFacesContextImpl. it does not have portlet-specific requirement right now.
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@988 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/context/portlet/PortletFacesContextImpl.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/context/portlet/PortletFacesContextImpl.java
@@ -15,7 +15,10 @@
  */
 package org.seasar.teeda.core.context.portlet;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.faces.application.Application;
 import javax.faces.application.FacesMessage;
@@ -25,113 +28,205 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseStream;
 import javax.faces.context.ResponseWriter;
+import javax.faces.internal.AssertionUtil;
 import javax.faces.render.RenderKit;
+
+import org.seasar.teeda.core.context.Releaseable;
+import org.seasar.teeda.core.util.ApplicationUtil;
+import org.seasar.teeda.core.util.FactoryFinderUtil;
 
 /**
  * @author shot
+ * @author shinsuke
  * 
  */
 public class PortletFacesContextImpl extends FacesContext {
 
-    //TODO impl these
-    
+    private ExternalContext externalContext;
+
+    private Application application;
+
+    private boolean released = false;
+
+    private boolean renderResponse = false;
+
+    private boolean responseComplete = false;
+
+    private ResponseWriter responseWriter;
+
+    private UIViewRoot root;
+
+    private List messages;
+
+    private List messageClientIds;
+
+    private FacesMessage.Severity maxSeverity;
+
+    private ResponseStream responseStream;
+
     public PortletFacesContextImpl(ExternalContext externalContext) {
-        
+        this.externalContext = externalContext;
+        this.application = ApplicationUtil.getApplicationFromFactory();
+        FacesContext.setCurrentInstance(this);
     }
-    
+
     public Application getApplication() {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        return application;
     }
 
     public Iterator getClientIdsWithMessages() {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        if (messages == null) {
+            return Collections.EMPTY_LIST.iterator();
+        }
+        return messageClientIds.iterator();
     }
 
     public ExternalContext getExternalContext() {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        return externalContext;
     }
 
     public Severity getMaximumSeverity() {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        return maxSeverity;
     }
 
     public Iterator getMessages() {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        return (messages != null) ? messages.iterator()
+                : Collections.EMPTY_LIST.iterator();
     }
 
     public Iterator getMessages(String clientId) {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        if (messages == null) {
+            return Collections.EMPTY_LIST.iterator();
+        }
+        if (messages.size() != messageClientIds.size()) {
+            throw new IllegalStateException();
+        }
+        List list = new ArrayList();
+        for (int i = 0; i < messages.size(); i++) {
+            String savedClientId = (String) messageClientIds.get(i);
+            if (stringEquals(clientId, savedClientId)) {
+                list.add(messages.get(i));
+            }
+        }
+        return list.iterator();
+    }
+
+    private boolean stringEquals(String a, String b) {
+        if (a == null) {
+            return b == null;
+        }
+        return a.equals(b);
     }
 
     public RenderKit getRenderKit() {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        UIViewRoot root = getViewRoot();
+        if (root == null) {
+            return null;
+        }
+        String renderKitId = root.getRenderKitId();
+        if (renderKitId == null) {
+            return null;
+        }
+        return FactoryFinderUtil.getRenderKitFactory().getRenderKit(this,
+                renderKitId);
     }
 
     public boolean getRenderResponse() {
-        // TODO Auto-generated method stub
-        return false;
+        assertNotReleased();
+        return renderResponse;
     }
 
     public boolean getResponseComplete() {
-        // TODO Auto-generated method stub
-        return false;
+        assertNotReleased();
+        return responseComplete;
     }
 
     public ResponseStream getResponseStream() {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        return responseStream;
     }
 
     public void setResponseStream(ResponseStream responseStream) {
-        // TODO Auto-generated method stub
-
+        assertNotReleased();
+        AssertionUtil.assertNotNull("responseStream", responseStream);
+        this.responseStream = responseStream;
     }
 
     public ResponseWriter getResponseWriter() {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        return responseWriter;
     }
 
     public void setResponseWriter(ResponseWriter responseWriter) {
-        // TODO Auto-generated method stub
-
+        assertNotReleased();
+        AssertionUtil.assertNotNull("responseWriter", responseWriter);
+        this.responseWriter = responseWriter;
     }
 
     public UIViewRoot getViewRoot() {
-        // TODO Auto-generated method stub
-        return null;
+        assertNotReleased();
+        return root;
     }
 
     public void setViewRoot(UIViewRoot root) {
-        // TODO Auto-generated method stub
-
+        assertNotReleased();
+        AssertionUtil.assertNotNull("root", root);
+        this.root = root;
     }
 
     public void addMessage(String clientId, FacesMessage message) {
-        // TODO Auto-generated method stub
-
+        assertNotReleased();
+        AssertionUtil.assertNotNull("message", message);
+        if (messages == null) {
+            messages = new ArrayList();
+            messageClientIds = new ArrayList();
+        }
+        messages.add(message);
+        messageClientIds.add(clientId);
+        FacesMessage.Severity severity = message.getSeverity();
+        setSeverity(severity);
     }
 
     public void release() {
-        // TODO Auto-generated method stub
-
+        released = true;
+        application = null;
+        if(externalContext instanceof Releaseable) {
+            ((Releaseable)externalContext).release();
+        }
+        externalContext = null;
+        FacesContext.setCurrentInstance(null);
     }
 
     public void renderResponse() {
-        // TODO Auto-generated method stub
-
+        assertNotReleased();
+        renderResponse = true;
     }
 
     public void responseComplete() {
-        // TODO Auto-generated method stub
-
+        assertNotReleased();
+        responseComplete = true;
     }
+
+    private void setSeverity(FacesMessage.Severity severity) {
+        if (severity != null) {
+            if (maxSeverity == null || severity.compareTo(maxSeverity) > 0) {
+                maxSeverity = severity;
+            }
+        }
+    }
+
+    private void assertNotReleased() {
+        if (released) {
+            throw new IllegalStateException("already released");
+        }
+    }
+
 
 }
