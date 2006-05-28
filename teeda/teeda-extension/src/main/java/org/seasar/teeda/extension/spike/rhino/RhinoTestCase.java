ddc4b6cd16cb57ead715e40ca3b7fb30aeb5449d
shot6 <shinpei.ohtani@gmail.com>
2006-05-28 16:31:19 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1010 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-extension/src/main/java/org/seasar/teeda/extension/spike/rhino/RhinoTestCase.java
+++ teeda/teeda-extension/src/main/java/org/seasar/teeda/extension/spike/rhino/RhinoTestCase.java
@@ -16,69 +16,75 @@ import org.mozilla.javascript.ScriptableObject;
 
 public class RhinoTestCase extends TestCase {
 
-	private ScriptableObject scope;
+    private ScriptableObject globalScope;
 
-	public RhinoTestCase() {
+    public RhinoTestCase() {
+    }
 
-	}
+    public RhinoTestCase(String name, ScriptableObject globalScope) {
+        super(name);
+        this.globalScope = globalScope;
+    }
+    
+    public Test createJsTestCase() throws Throwable {
 
-	public RhinoTestCase(String testName, ScriptableObject scope) {
-		super(testName);
-		this.setName(testName);
-		this.scope = scope;
-	}
-	/*
-	public void testScript() {
+        String path = this.getClass().getName();
+        path = path.replace('.', '/') + ".js";
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        InputStream in = null;
+        Reader reader = null;
+        TestSuite suite = new TestSuite(path);
+        try {
+            in = loader.getResourceAsStream(path);
+            reader = new InputStreamReader(in, "UTF-8");
+            Context cx = Context.enter();
+            ScriptableObject scope = cx.initStandardObjects();
 
-	}*/
+            builtinAssertMethods(cx, scope);
+            loadScript(cx, scope);
 
-	public Test createJsTestCase() throws Throwable {
+            Script script = cx.compileReader(reader, new File(path).getName(),
+                    0, null);
+            Object result = script.exec(cx, scope);
+            Object[] list = scope.getIds();
+            Context.exit();
+            for (int i = 0; i < list.length; i++) {
+                String name = list[i].toString();
+                if (name.toString().startsWith("test")) {
+                    Object o = scope.get(name, null);
+                    Function f = (Function) o;
+                    suite.addTest(new RhinoTestCase(name, scope));
+                }
+            }
+            return suite;
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
 
-		String path = this.getClass().getName();
-		path = path.replace('.', '/') + ".js";
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		InputStream in = null;
-		Reader reader = null;
-		TestSuite suite = new TestSuite(path);
-		try {
-			in = loader.getResourceAsStream(path);
-			reader = new InputStreamReader(in, "UTF-8");
-			Context cx = Context.enter();
-			ScriptableObject scope = cx.initStandardObjects();
+    protected ScriptableObject getGlobalScope() {
+        return globalScope;
+    }
 
-			String assert = "var assert = Packages.junit.framework.Assert; for(var v in assert){ this[v] = assert[v];}";
-			cx.evaluateString(scope, assert, "assert", 0, null);
-			String scriptLoader = "this.loadScript = function loadScript(name){Packages.org.seasar.teeda.extension.spike.rhino.JavascriptLoader.load(name, this);}";
-			cx.evaluateString(scope, scriptLoader, "scriptLoader", 0, null);
+    protected void runTest() throws Throwable {
+        Context cx = Context.enter();
+        Object o = getGlobalScope().get(this.getName(), null);
+        Function func = (Function) o;
+        func.call(cx, getGlobalScope(), getGlobalScope(), null);
+        Context.exit();
+    }
 
-			Script script = cx.compileReader(reader, new File(path).getName(),
-					0, null);
-			Object result = script.exec(cx, scope);
-			Object[] list = scope.getIds();
-			cx.exit();
-			for (int i = 0; i < list.length; i++) {
-				String name = list[i].toString();
-				if (name.toString().startsWith("test")) {
-					Object o = scope.get(name, null);
-					Function f = (Function) o;
-					RhinoTestCase test = new RhinoTestCase(name, scope);
-					suite.addTest(test);
-				}
-			}
-			return suite;
-		} finally {
-			if (in != null) {
-				in.close();
-			}
-		}
-	}
+    //TODO use ScriptableObject.defineFunctionProperties()
+    protected void builtinAssertMethods(Context cx, ScriptableObject scope) {
+        String assertStr = "var assert = Packages.junit.framework.Assert; for(var v in assert){ this[v] = assert[v];}";
+        cx.evaluateString(scope, assertStr, "assert", 0, null);
+    }
 
-	protected void runTest() throws Throwable {
-		Context cx = Context.enter();
-		Object o = scope.get(this.getName(), null);
-		Function func = (Function) o;
-		func.call(cx, scope, scope, null);
-		cx.exit();
-	}
-
+    //TODO use ScriptableObject.defineFunctionProperties()
+    protected void loadScript(Context cx, ScriptableObject scope) {
+        String scriptLoader = "this.loadScript = function loadScript(name){Packages.org.seasar.teeda.extension.spike.rhino.JavascriptLoader.load(name, this);}";
+        cx.evaluateString(scope, scriptLoader, "scriptLoader", 0, null);
+    }
 }
