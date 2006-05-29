461b08cf9fe9ed69f26f62ef4e774cd48a0c03ca
shot6 <shinpei.ohtani@gmail.com>
2006-05-29 15:11:57 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1015 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-extension/src/test/java/org/seasar/teeda/extension/spike/rhino/Target2Test.java
@@ -0,0 +1,11 @@
+package org.seasar.teeda.extension.spike.rhino;
+
+import junit.framework.Test;
+
+public class Target2Test extends RhinoTestCase {
+
+    public static Test suite() throws Throwable {
+        return new Target2Test().createJsTestCase();
+    }
+
+}
