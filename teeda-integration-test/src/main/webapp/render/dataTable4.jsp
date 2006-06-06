d12f69ff7cf4abf43d1848b41fe2f70d14385e2b
manhole <manholex@gmail.com>
2006-06-06 10:13:39 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1084 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/src/main/webapp/render/dataTable4.jsp
@@ -0,0 +1,37 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsf/core" prefix="f"%>
+<%@ taglib uri="http://java.sun.com/jsf/html" prefix="h"%>
+<html>
+<head>
+<title>dataTable4.jsp</title>
+</head>
+<body>
+<f:view>
+	<h:form id="dataTableForm">
+		<h:dataTable value="#{dataTableBean.items}" var="item" border="1">
+			<f:facet name="header">
+				<h:outputText value="products" />
+			</f:facet>
+			<f:facet name="footer">
+				<h:panelGroup>
+					<h:outputText value="#{dataTableBean.itemSize}" />
+					<f:verbatim> items.</f:verbatim>
+				</h:panelGroup>
+			</f:facet>
+			<h:column>
+				<f:facet name="header">
+					<h:outputText value="name" />
+				</f:facet>
+				<h:outputText value="#{item.name}" />
+			</h:column>
+			<h:column>
+				<f:facet name="header">
+					<h:outputText value="price" />
+				</f:facet>
+				<h:inputText value="#{item.price}" />
+			</h:column>
+		</h:dataTable>
+	</h:form>
+</f:view>
+</body>
+</html>
---
+++ teeda-integration-test/src/test/java/org/seasar/teeda/it/render/DataTableInputTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it.render;
+
+import java.net.URL;
+
+import junit.framework.Test;
+
+import org.seasar.teeda.it.AbstractTestCase;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+/**
+ * @author manhole
+ */
+public class DataTableInputTest extends AbstractTestCase {
+
+    public static Test suite() throws Exception {
+        return setUpTestSuite(DataTableInputTest.class);
+    }
+
+    public void testRender4() throws Exception {
+        // ## Arrange ##
+        URL url = getUrl("faces/render/dataTable4.jsp");
+        System.out.println(url);
+
+        WebClient webClient = new WebClient();
+
+        // ## Act ##
+        HtmlPage page = (HtmlPage) webClient.getPage(url);
+
+        // ## Assert ##
+        final String body1 = getBody(page).trim();
+        System.out.println(body1);
+        assertEquals("dataTable4.jsp", page.getTitleText());
+
+    }
+
+ 
+
+}
