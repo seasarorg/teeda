43b0354f1667e8710433135ab5354b9980bf597c
shot6 <shinpei.ohtani@gmail.com>
2006-06-23 00:36:02 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1206 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-extension/src/main/java/org/seasar/teeda/extension/validator/RequiredValidator.java
+++ teeda/teeda-extension/src/main/java/org/seasar/teeda/extension/validator/RequiredValidator.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -21,12 +21,12 @@ import javax.faces.component.StateHolder;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.internal.FacesMessageUtils;
+import javax.faces.internal.UIComponentUtil;
 import javax.faces.internal.UIInputUtil;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
 import org.seasar.framework.util.AssertionUtil;
-import org.seasar.teeda.core.util.UIComponentUtil;
 
 /**
  * @author higa
