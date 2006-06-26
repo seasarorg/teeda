444b83658898b1ed35144514463ec429e0bc92d5
Yasuo Higa <higayasuo@gmail.com>
2006-06-26 12:16:32 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1229 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-extension-example/WEB-INF/src/examples/teeda/web/add/AddPage.java
@@ -0,0 +1,41 @@
+package examples.teeda.web.add;
+
+public class AddPage {
+
+	public static final String arg1_VALIDATOR = "#{'validator':'lengthValidator','minimum':3}";
+    
+	private int arg1;
+    
+    private int arg2;
+    
+    private int result;
+
+    public int getArg1() {
+        return arg1;
+    }
+
+    public void setArg1(int arg1) {
+        this.arg1 = arg1;
+    }
+
+    public int getArg2() {
+        return arg2;
+    }
+
+    public void setArg2(int arg2) {
+        this.arg2 = arg2;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+    
+    public String doCalculate() {
+        result = arg1 + arg2;
+        return null;
+    }
+}
---
+++ teeda-extension-example/WEB-INF/src/examples/teeda/web/hello/HelloPage.java
@@ -0,0 +1,14 @@
+package examples.teeda.web.hello;
+
+public class HelloPage {
+
+	private String name = "Seasar2";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
