116d6bfb72c6510939a9f37a69dcba1bce1bca09
shot6 <shinpei.ohtani@gmail.com>
2006-06-29 16:17:44 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1260 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/main/java/javax/faces/internal/InternalConstants.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package javax.faces.internal;
+
+/**
+ * @author shot
+ * 
+ */
+public interface InternalConstants {
+
+    String COMPONENT_TAG_STACK_ATTR = "javax.faces.webapp.COMPONENT_TAG_STACK";
+
+    String JSP_CREATED_COMPONENT_IDS = "javax.faces.webapp.COMPONENT_IDS";
+
+    String JSP_CREATED_FACET_NAMES = "javax.faces.webapp.FACET_NAMES";
+
+    String GLOBAL_ID_VIEW = "javax.faces.webapp.GLOBAL_ID_VIEW";
+
+    String CURRENT_FACES_CONTEXT = "javax.faces.webapp.CURRENT_FACES_CONTEXT";
+
+    String CURRENT_VIEW_ROOT = "javax.faces.webapp.CURRENT_VIEW_ROOT";
+
+}
