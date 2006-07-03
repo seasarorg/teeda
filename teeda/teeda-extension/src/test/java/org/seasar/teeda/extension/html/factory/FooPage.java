22b6553a614d3c25a118e65e040903c76c814886
Yasuo Higa <higayasuo@gmail.com>
2006-07-03 08:20:49 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1292 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-extension/src/test/java/org/seasar/teeda/extension/html/factory/FooPage.java
+++ teeda/teeda-extension/src/test/java/org/seasar/teeda/extension/html/factory/FooPage.java
@@ -1,12 +1,44 @@
 package org.seasar.teeda.extension.html.factory;
 
+import java.util.List;
+
 public class FooPage {
+
+    private int hogeNo;
+
+    private String hogeNama;
+
+    private List hogeItems;
 
     public String getAaa() {
         return null;
     }
-    
+
     public String doBbb() {
         return null;
+    }
+
+    public String getHogeNama() {
+        return hogeNama;
+    }
+
+    public void setHogeNama(String hogeNama) {
+        this.hogeNama = hogeNama;
+    }
+
+    public int getHogeNo() {
+        return hogeNo;
+    }
+
+    public void setHogeNo(int hogeNo) {
+        this.hogeNo = hogeNo;
+    }
+
+    public List getHogeItems() {
+        return hogeItems;
+    }
+
+    public void setHogeItems(List hogeItems) {
+        this.hogeItems = hogeItems;
     }
 }
---
+++ teeda/teeda-extension/src/test/java/org/seasar/teeda/extension/html/factory/ForEachFactoryTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.extension.html.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.seasar.teeda.core.unit.TeedaTestCase;
+import org.seasar.teeda.extension.ExtensionConstants;
+import org.seasar.teeda.extension.config.taglib.element.TagElement;
+import org.seasar.teeda.extension.config.taglib.element.TaglibElement;
+import org.seasar.teeda.extension.config.taglib.element.impl.TagElementImpl;
+import org.seasar.teeda.extension.config.taglib.element.impl.TaglibElementImpl;
+import org.seasar.teeda.extension.html.ElementNode;
+import org.seasar.teeda.extension.html.ElementProcessor;
+import org.seasar.teeda.extension.html.PageDesc;
+import org.seasar.teeda.extension.html.impl.ElementNodeImpl;
+import org.seasar.teeda.extension.html.impl.PageDescImpl;
+import org.seasar.teeda.extension.mock.MockTaglibManager;
+import org.seasar.teeda.extension.taglib.TForEachTag;
+
+/**
+ * @author higa
+ */
+public class ForEachFactoryTest extends TeedaTestCase {
+
+    public void testIsMatch() throws Exception {
+        ForEachFactory factory = new ForEachFactory();
+        Map properties = new HashMap();
+        properties.put("id", "hogeItems");
+        ElementNode elementNode = new ElementNodeImpl("div", properties);
+        PageDesc pageDesc = new PageDescImpl(FooPage.class, "fooPage");
+        assertTrue(factory.isMatch(elementNode, pageDesc, null));
+        ElementNode elementNode2 = new ElementNodeImpl("hoge", properties);
+        assertFalse(factory.isMatch(elementNode2, pageDesc, null));
+        properties.put("id", "xxx");
+        ElementNode elementNode3 = new ElementNodeImpl("input", properties);
+        assertFalse(factory.isMatch(elementNode3, pageDesc, null));
+    }
+
+    public void testCreateFactory() throws Exception {
+        // ## Arrange ##
+        MockTaglibManager taglibManager = new MockTaglibManager();
+        TaglibElement taglibElem = new TaglibElementImpl();
+        taglibElem.setUri(ExtensionConstants.TEEDA_EXTENSION_URI);
+        TagElement tagElement = new TagElementImpl();
+        tagElement.setName("forEach");
+        tagElement.setTagClass(TForEachTag.class);
+        taglibElem.addTagElement(tagElement);
+        taglibManager.addTaglibElement(taglibElem);
+        ForEachFactory factory = new ForEachFactory();
+        factory.setTaglibManager(taglibManager);
+        Map properties = new HashMap();
+        properties.put("id", "hogeItems");
+        ElementNode elementNode = new ElementNodeImpl("div", properties);
+        PageDesc pageDesc = new PageDescImpl(FooPage.class, "fooPage");
+
+        // ## Act ##
+        ElementProcessor processor = factory.createProcessor(elementNode,
+                pageDesc, null);
+        // ## Assert ##
+        assertNotNull(processor);
+        assertEquals(TForEachTag.class, processor.getTagClass());
+        assertEquals("fooPage", processor
+                .getProperty(ExtensionConstants.PAGE_NAME_ATTR));
+        assertEquals("hogeItems", processor
+                .getProperty(ExtensionConstants.ITEMS_NAME_ATTR));
+    }
+}
