3ae705937651253b32fc243ad0084e92b31d4b3f
shot6 <shinpei.ohtani@gmail.com>
2006-07-03 15:43:26 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1293 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-ajax/src/main/java/org/seasar/teeda/ajax/autoregister/AjaxComponentCustomizer.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.ajax.autoregister;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.seasar.framework.beans.BeanDesc;
+import org.seasar.framework.beans.factory.BeanDescFactory;
+import org.seasar.framework.container.ComponentDef;
+import org.seasar.framework.container.autoregister.ComponentCustomizer;
+import org.seasar.framework.container.impl.MetaDefImpl;
+import org.seasar.teeda.core.util.PatternUtil;
+
+/**
+ * @author shot
+ * 
+ */
+public class AjaxComponentCustomizer implements ComponentCustomizer {
+
+    private List methodNames = new ArrayList();
+
+    public AjaxComponentCustomizer() {
+        addMethodPattern("ajaxAction");
+    }
+
+    public void customize(ComponentDef componentDef) {
+        BeanDesc beanDesc = BeanDescFactory.getBeanDesc(componentDef
+                .getComponentClass());
+        String[] names = beanDesc.getMethodNames();
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+            if(isMatch(name)) {
+                componentDef.addMetaDef(new MetaDefImpl("teeda-ajax"));
+            }
+        }
+    }
+
+    public void addMethodPattern(String methodName) {
+        this.methodNames.add(PatternUtil.getPattern(methodName));
+    }
+    
+    protected boolean isMatch(String name) {
+        for(Iterator itr = methodNames.iterator(); itr.hasNext();) {
+            Pattern pattern = (Pattern) itr.next();
+            if(pattern.matcher(name).matches()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
