e7151120e3799a6f07ef388ece13d1c46d6fe692
manhole <manholex@gmail.com>
2006-07-25 04:34:17 +0000

    add grid
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1428 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-jsp-example/src/main/java/examples/teeda/bean/GridBean.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.teeda.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author manhole
+ */
+public class GridBean {
+
+    private Item[] fooItems;
+
+    private Integer fooIndex;
+
+    public Item[] getFooItems() {
+        if (fooItems == null) {
+            List l = new ArrayList();
+            l.add(createItem("aaa1", "bbbb1", "ccccc1"));
+            l.add(createItem("aaa2", "bbbb2", "ccccc2"));
+            l.add(createItem("aaa3", "bbbb3", "ccccc3"));
+            l.add(createItem("aaa4", "bbbb4", "ccccc4"));
+            l.add(createItem("aaa5", "bbbb5", "ccccc5"));
+            l.add(createItem("aaa6", "bbbb6", "ccccc6"));
+            l.add(createItem("aaa7", "bbbb7", "ccccc7"));
+            fooItems = (Item[]) l.toArray(new Item[l.size()]);
+        }
+        return fooItems;
+    }
+
+    public void setFooItems(Item[] fooItems) {
+        this.fooItems = fooItems;
+    }
+
+    private Item createItem(String aaa, String bbb, String ccc) {
+        Item item = new Item();
+        item.setAaa(aaa);
+        item.setBbb(bbb);
+        item.setCcc(ccc);
+        item.setDdd(ccc + ", " + bbb + ", " + aaa);
+        return item;
+    }
+
+    public static class Item {
+
+        private String aaa;
+
+        private String bbb;
+
+        private String ccc;
+
+        private String ddd;
+
+        public String getAaa() {
+            return aaa;
+        }
+
+        public void setAaa(String aaa) {
+            this.aaa = aaa;
+        }
+
+        public String getBbb() {
+            return bbb;
+        }
+
+        public void setBbb(String bbb) {
+            this.bbb = bbb;
+        }
+
+        public String getCcc() {
+            return ccc;
+        }
+
+        public void setCcc(String ccc) {
+            this.ccc = ccc;
+        }
+
+        public String getDdd() {
+            return ddd;
+        }
+
+        public void setDdd(String ddd) {
+            this.ddd = ddd;
+        }
+    }
+
+    private String aaa;
+
+    private String bbb;
+
+    private String ccc;
+
+    private String ddd;
+
+    public String getAaa() {
+        return aaa;
+    }
+
+    public void setAaa(String aaa) {
+        this.aaa = aaa;
+    }
+
+    public String getBbb() {
+        return bbb;
+    }
+
+    public void setBbb(String bbb) {
+        this.bbb = bbb;
+    }
+
+    public String getCcc() {
+        return ccc;
+    }
+
+    public void setCcc(String ccc) {
+        this.ccc = ccc;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public Integer getFooIndex() {
+        return fooIndex;
+    }
+
+    public void setFooIndex(Integer fooIndex) {
+        this.fooIndex = fooIndex;
+    }
+
+}
---
+++ teeda-jsp-example/src/main/webapp/grid/grid.jsp
@@ -0,0 +1,67 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsf/core" prefix="f"%>
+<%@ taglib uri="http://java.sun.com/jsf/html" prefix="h"%>
+<%@ taglib uri="http://www.seasar.org/teeda/extension" prefix="te"%>
+<html>
+<head>
+<title>grid.jsp</title>
+<style>
+.gridHeader {
+	background-color: green;
+	color: white;
+	font-weight: bold;
+	overflow: hidden;
+}
+.gridTable {
+	background-color: black;
+}
+.gridCell {
+	background-color: white;
+	overflow: hidden;
+	height: 20px;
+	padding: 0px;
+	margin: 0px;
+}
+</style>
+</head>
+
+<body>
+<f:view>
+<h:form id="inputTextForm">
+	<%--
+		fooGridXY
+		-> fooItemsがCollectionであることを期待
+		-> fooItemsの各要素をfooへ置く
+	--%>
+	<te:grid id="fooGridXY" pageName="gridBean" itemsName="fooItems" width="250px" height="200px">
+		<te:gridColGroup styleClass="aaa bbb teeda_leftFixed">
+			<te:gridCol span="1" width="10px" styleClass="teeda_leftFixed" />
+			<te:gridCol span="1" width="50px" styleClass="teeda_leftFixed" />
+			<te:gridCol span="4" width="70px" />
+		</te:gridColGroup>
+		<te:gridHeader>
+			<te:gridTr height="30px">
+				<te:gridTh><h:outputText value="&nbsp;" escape="false"/></te:gridTh>
+				<te:gridTh><h:outputText value="th1"/></te:gridTh>
+				<te:gridTh><h:outputText value="th2"/></te:gridTh>
+				<te:gridTh><h:outputText value="th3"/></te:gridTh>
+				<te:gridTh><h:outputText value="th4"/></te:gridTh>
+				<te:gridTh><h:outputText value="th5"/></te:gridTh>
+			</te:gridTr>
+		</te:gridHeader>
+		<te:gridBody>
+			<te:gridTr>
+				<te:gridTd><h:outputText value="#{gridBean.fooIndex + 1}"/></te:gridTd>
+				<te:gridTd><h:outputText value="#{gridBean.aaa}"/><h:inputHidden value="#{gridBean.aaa}"/></te:gridTd>
+				<te:gridTd><h:outputText value="#{gridBean.bbb}"/></te:gridTd>
+				<te:gridTd><te:gridInputText value="#{gridBean.bbb}"/></te:gridTd>
+				<te:gridTd><h:outputText value="#{gridBean.ccc}"/><h:inputHidden value="#{gridBean.ccc}"/></te:gridTd>
+				<te:gridTd><h:outputText value="#{gridBean.ddd}"/><h:inputHidden value="#{gridBean.ddd}"/></te:gridTd>
+			</te:gridTr>
+		</te:gridBody>
+	</te:grid>
+	<h:commandButton id="submit1"/>
+</h:form>
+</f:view>
+</body>
+</html>
