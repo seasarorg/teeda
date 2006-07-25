f723ac7f77606c668a8101b682140b67d80615e5
shot6 <shinpei.ohtani@gmail.com>
2006-07-25 11:51:28 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1440 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/WEB-INF/src/examples/teeda/web/select/SelectOnePage.java
@@ -0,0 +1,48 @@
+package examples.teeda.web.select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectOnePage {
+
+	// public static final String SELECT_SELECTRESULTPAGE =
+	// "select_selectOneResultPage";
+
+	private List aaaItems;
+
+	private int aaa;
+
+	public String initialize() {
+		aaaItems = new ArrayList();
+		AaaDto dto1 = new AaaDto();
+		dto1.setValue(0);
+		dto1.setLabel("AAAA");
+		aaaItems.add(dto1);
+		AaaDto dto2 = new AaaDto();
+		dto2.setValue(1);
+		dto2.setLabel("BBBB");
+		aaaItems.add(dto2);
+		AaaDto dto3 = new AaaDto();
+		dto3.setValue(2);
+		dto3.setLabel("CCCC");
+		aaaItems.add(dto3);
+		return null;
+	}
+
+	public List getAaaItems() {
+		return aaaItems;
+	}
+
+	public void setAaaItems(List aaaItems) {
+		this.aaaItems = aaaItems;
+	}
+
+	public int getAaa() {
+		return aaa;
+	}
+
+	public void setAaa(int aaa) {
+		this.aaa = aaa;
+	}
+
+}
