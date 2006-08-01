53cfa6be00a8408a9ecefbb20e319ab60fd53eff
shot6 <shinpei.ohtani@gmail.com>
2006-08-01 05:33:50 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1504 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/WEB-INF/src/examples/teeda/web/radio/SelectOneRadioPage.java
@@ -0,0 +1,51 @@
+/**
+ *
+ */
+package examples.teeda.web.radio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author shot
+ */
+public class SelectOneRadioPage {
+
+	private int aaa;
+
+	private List aaaItems;
+
+	public String initialize() {
+		aaaItems = new ArrayList();
+		AaaDto dto1 = new AaaDto();
+		dto1.setValue(0);
+		dto1.setLabel("AAAA");
+		aaaItems.add(dto1);
+		AaaDto dto2 = new AaaDto();
+		dto2.setValue(1);
+		dto2.setLabel("BBBB");
+		aaaItems.add(dto2);
+		AaaDto dto3 = new AaaDto();
+		dto3.setValue(2);
+		dto3.setLabel("CCCC");
+		aaaItems.add(dto3);
+		return null;
+	}
+
+	public int getAaa() {
+		return aaa;
+	}
+
+	public void setAaa(int aaa) {
+		this.aaa = aaa;
+	}
+
+	public List getAaaItems() {
+		return aaaItems;
+	}
+
+	public void setAaaItems(List aaaItems) {
+		this.aaaItems = aaaItems;
+	}
+
+}
