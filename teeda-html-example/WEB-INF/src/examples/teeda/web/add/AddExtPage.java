36c9eb05324179546df75af6575a2370ba9dee7d
shot6 <shinpei.ohtani@gmail.com>
2006-08-07 02:35:04 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1550 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/WEB-INF/src/examples/teeda/web/add/AddExtPage.java
@@ -0,0 +1,67 @@
+package examples.teeda.web.add;
+
+import java.math.BigDecimal;
+
+public class AddExtPage {
+
+	private BigDecimal arg1;
+
+	private BigDecimal arg2;
+
+	private BigDecimal result;
+
+	private int arg1Fraction = 4;
+
+	private int arg2Fraction = 2;
+
+	public int getArg1Fraction() {
+		return arg1Fraction;
+	}
+
+	public int getArg2Fraction() {
+		return arg2Fraction;
+	}
+
+	public void setArg1Fraction(int arg1Fraction) {
+		this.arg1Fraction = arg1Fraction;
+	}
+
+	public void setArg2Fraction(int arg2Fraction) {
+		this.arg2Fraction = arg2Fraction;
+	}
+
+	public BigDecimal getArg1() {
+		return arg1;
+	}
+
+	public void setArg1(BigDecimal arg1) {
+		this.arg1 = arg1;
+	}
+
+	public BigDecimal getArg2() {
+		return arg2;
+	}
+
+	public void setArg2(BigDecimal arg2) {
+		this.arg2 = arg2;
+	}
+
+	public BigDecimal getResult() {
+		return result;
+	}
+
+	public void setResult(BigDecimal result) {
+		this.result = result;
+	}
+
+	public String doCalculate() {
+		if (arg1 == null) {
+			arg1 = new BigDecimal("0");
+		}
+		if (arg2 == null) {
+			arg2 = new BigDecimal("0");
+		}
+		result = arg1.add(arg2);
+		return null;
+	}
+}
