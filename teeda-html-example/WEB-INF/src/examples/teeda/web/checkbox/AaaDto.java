b97523973ea7a5509f2b6f005a1766fdd2ee101e
shot6 <shinpei.ohtani@gmail.com>
2006-08-11 09:43:06 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1589 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/WEB-INF/src/examples/teeda/web/checkbox/AaaDto.java
@@ -0,0 +1,24 @@
+package examples.teeda.web.checkbox;
+
+public class AaaDto {
+
+	private String label;
+
+	private int value;
+
+	public String getLabel() {
+		return label;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
