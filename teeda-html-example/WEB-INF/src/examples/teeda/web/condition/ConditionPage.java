4b886919220f6ee83db0b63b7c9381fc5ca366f0
shot6 <shinpei.ohtani@gmail.com>
2006-08-11 04:32:44 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1584 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/WEB-INF/src/examples/teeda/web/condition/ConditionPage.java
@@ -0,0 +1,41 @@
+/**
+ *
+ */
+package examples.teeda.web.condition;
+
+/**
+ * @author shot
+ */
+public class ConditionPage {
+
+	private boolean aaa = true;
+
+	private String bbb = "BBB";
+
+	private String ccc = "CCC";
+
+	public boolean isAaa() {
+		return aaa;
+	}
+
+	public String getBbb() {
+		return bbb;
+	}
+
+	public String getCcc() {
+		return ccc;
+	}
+
+	public void setAaa(boolean aaa) {
+		this.aaa = aaa;
+	}
+
+	public void setBbb(String bbb) {
+		this.bbb = bbb;
+	}
+
+	public void setCcc(String ccc) {
+		this.ccc = ccc;
+	}
+
+}
