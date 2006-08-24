ac02765c9058f4b1c969fae773e5e434c1a54545
Yasuo Higa <higayasuo@gmail.com>
2006-08-24 11:48:55 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1688 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/grid/GridResultPage.java
@@ -0,0 +1,15 @@
+package examples.teeda.web.grid;
+
+public class GridResultPage {
+
+	private String fff;
+
+	public String getFff() {
+		return fff;
+	}
+
+	public void setFff(String fff) {
+		this.fff = fff;
+	}
+
+}
