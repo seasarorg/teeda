91b11493728003c6d7d7b491dcea1f40e34bee50
shot6 <shinpei.ohtani@gmail.com>
2006-08-25 13:09:26 +0000

    add label.
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1696 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/label/LabelPage.java
@@ -0,0 +1,15 @@
+package examples.teeda.web.label;
+
+public class LabelPage {
+
+	private String name = "Foo";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
