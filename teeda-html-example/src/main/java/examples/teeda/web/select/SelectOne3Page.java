847d1b8bde8244c01a9846c6456a402ae37ad770
shot6 <shinpei.ohtani@gmail.com>
2006-09-05 13:58:22 +0000

    select and forEach, hidden sample.
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1775 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/select/SelectOne3Page.java
@@ -0,0 +1,91 @@
+package examples.teeda.web.select;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SelectOne3Page {
+
+	private List aaaItems;
+
+	private int aaa;
+
+	private String bbb;
+
+	private List cccItems;
+
+	private String name;
+
+	public String initialize() {
+		aaaItems = new ArrayList();
+		Map map1 = new HashMap();
+		map1.put("label", "AAAA");
+		map1.put("value", new Integer(0));
+		aaaItems.add(map1);
+		Map map2 = new HashMap();
+		map2.put("label", "BBBB");
+		map2.put("value", new Integer(1));
+		aaaItems.add(map2);
+		Map map3 = new HashMap();
+		map3.put("label", "CCCC");
+		map3.put("value", new Integer(2));
+		aaaItems.add(map3);
+		cccItems = new ArrayList();
+		Map mapCcc1 = new HashMap();
+		mapCcc1.put("name", "name1");
+		cccItems.add(mapCcc1);
+		Map mapCcc2 = new HashMap();
+		mapCcc2.put("name", "name2");
+		cccItems.add(mapCcc2);
+		Map mapCcc3 = new HashMap();
+		mapCcc3.put("name", "name3");
+		cccItems.add(mapCcc3);
+		return null;
+	}
+
+	public List getAaaItems() {
+		return aaaItems;
+	}
+
+	public void setAaaItems(List aaaItems) {
+		this.aaaItems = aaaItems;
+	}
+
+	public int getAaa() {
+		return aaa;
+	}
+
+	public void setAaa(int aaa) {
+		this.aaa = aaa;
+	}
+
+	public String getBbb() {
+		return bbb;
+	}
+
+	public void setBbb(String bbb) {
+		this.bbb = bbb;
+	}
+
+	public String doAction() {
+		return null;
+	}
+
+	public List getCccItems() {
+		return cccItems;
+	}
+
+	public void setCccItems(List cccItems) {
+		this.cccItems = cccItems;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
