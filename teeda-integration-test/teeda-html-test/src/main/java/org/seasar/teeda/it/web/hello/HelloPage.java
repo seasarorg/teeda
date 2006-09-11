bac4862ba3aa9fc681e79431eae0ac77974b7ad1
manhole <manholex@gmail.com>
2006-09-11 14:45:32 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1831 e0972215-f904-0410-97d3-99f68b247671

--- teeda-integration-test/teeda-html-test/src/main/java/org/seasar/teeda/it/web/hello/HelloPage.java
+++ teeda-integration-test/teeda-html-test/src/main/java/org/seasar/teeda/it/web/hello/HelloPage.java
@@ -2,10 +2,6 @@ package org.seasar.teeda.it.web.hello;
 
 public class HelloPage {
 
-    public HelloPage() {
-        System.out.println("HelloPage.enclosing_method()");
-    }
-
     private String aaa = "Teeda Extension Tests";
 
     public String getAaa() {
