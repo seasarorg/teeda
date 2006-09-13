85fe73a05853d668dc67c58c1aef553760acd320
shot6 <shinpei.ohtani@gmail.com>
2006-09-13 05:14:30 +0000

    add LesserEqaulValidator, LesserValidator.
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1848 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-extension/src/main/java/org/seasar/teeda/extension/validator/TLesserEqualValidator.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.extension.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.internal.FacesMessageUtil;
+import javax.faces.internal.UIComponentUtil;
+import javax.faces.validator.ValidatorException;
+
+/**
+ * @author shot
+ */
+public class TLesserEqualValidator extends AbstractCompareValidator {
+
+    public static final String LE_MESSAGE_ID = "org.seasar.teeda.extension.validator.TLesserEqualValidator.LE";
+
+    protected void doValidate(FacesContext context, UIComponent component,
+            Object value, UIComponent targetComponent, Object targetValue)
+            throws ValidatorException {
+        if (targetValue == null) {
+            return;
+        }
+        if (isGreaterThan(value, targetValue)) {
+            Object[] args = { UIComponentUtil.getLabel(targetComponent),
+                    UIComponentUtil.getLabel(component) };
+            String messaId = getMessageId();
+            FacesMessage message = FacesMessageUtil.getMessage(context,
+                    messaId, args);
+            throw new ValidatorException(message, messaId, args);
+        }
+    }
+
+    public String getMessageId() {
+        String msg = super.getMessageId();
+        return (msg != null) ? msg : LE_MESSAGE_ID;
+    }
+
+    private boolean isGreaterThan(Object value, Object targetValue) {
+        return (!(value instanceof Comparable))
+                || ((Comparable) value).compareTo(targetValue) > 0;
+    }
+
+}
---
+++ teeda/teeda-extension/src/test/java/org/seasar/teeda/extension/validator/TLesserEqualValidatorTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.extension.validator;
+
+import javax.faces.component.UIInput;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.IntegerConverter;
+import javax.faces.validator.ValidatorException;
+
+import org.seasar.teeda.core.unit.TeedaTestCase;
+
+/**
+ * @author shot
+ */
+public class TLesserEqualValidatorTest extends TeedaTestCase {
+
+    public void testValidate_invalid() throws Exception {
+        TLesserEqualValidator validator = new TLesserEqualValidator();
+        FacesContext context = getFacesContext();
+        UIViewRoot root = new UIViewRoot();
+        UIInput child1 = new UIInput();
+        child1.setId("aaa");
+        child1.setValue(new Integer(11));
+        UIInput child2 = new UIInput();
+        child2.setId("bbb");
+        root.getChildren().add(child1);
+        root.getChildren().add(child2);
+
+        getApplication().addConverter(Integer.class,
+                IntegerConverter.class.getName());
+
+        Integer value = new Integer("123");
+        validator.setTargetId("aaa");
+
+        try {
+            validator.validate(context, child2, value);
+            fail();
+        } catch (ValidatorException expected) {
+            assertNotNull(expected);
+            System.out.println(expected.getMessage());
+            success();
+        }
+    }
+
+    public void testValidate_equalValueIsOk() throws Exception {
+        TLesserEqualValidator validator = new TLesserEqualValidator();
+        FacesContext context = getFacesContext();
+        UIViewRoot root = new UIViewRoot();
+        UIInput child1 = new UIInput();
+        child1.setId("aaa");
+        child1.setValue(new Integer(234));
+        UIInput child2 = new UIInput();
+        child2.setId("bbb");
+        root.getChildren().add(child1);
+        root.getChildren().add(child2);
+
+        getApplication().addConverter(Integer.class,
+                IntegerConverter.class.getName());
+
+        Integer value = new Integer("234");
+        validator.setTargetId("aaa");
+
+        try {
+            validator.validate(context, child2, value);
+            success();
+        } catch (ValidatorException expected) {
+            fail();
+        }
+    }
+
+    public void testValidate_valid() throws Exception {
+        TLesserEqualValidator validator = new TLesserEqualValidator();
+        FacesContext context = getFacesContext();
+        UIViewRoot root = new UIViewRoot();
+        UIInput child1 = new UIInput();
+        child1.setId("aaa");
+        child1.setValue(new Integer(234));
+        UIInput child2 = new UIInput();
+        child2.setId("bbb");
+        root.getChildren().add(child1);
+        root.getChildren().add(child2);
+
+        getApplication().addConverter(Integer.class,
+                IntegerConverter.class.getName());
+
+        Integer value = new Integer("123");
+        validator.setTargetId("aaa");
+
+        try {
+            validator.validate(context, child2, value);
+            success();
+        } catch (ValidatorException expected) {
+            fail();
+        }
+    }
+
+}
