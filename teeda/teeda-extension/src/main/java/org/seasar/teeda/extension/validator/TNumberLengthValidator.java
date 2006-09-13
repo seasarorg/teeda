da8c9644608abd2e9ae70ce51172ab20aae98d5c
manhole <manholex@gmail.com>
2006-09-13 23:37:36 +0000

    https://www.seasar.org/issues/browse/TEEDA-89
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1863 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-extension/src/main/java/org/seasar/teeda/extension/validator/TNumberLengthValidator.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.extension.validator;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+
+import javax.faces.FacesException;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.internal.FacesMessageUtil;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.seasar.framework.util.AssertionUtil;
+import org.seasar.framework.util.NumberConversionUtil;
+
+/**
+ * @author manhole
+ */
+public class TNumberLengthValidator implements Validator {
+
+    /*
+     * String型で"ab.cd"と入ってきた場合も、このバリデータでは桁数しか見ない。
+     * 数値であって欲しい場合はプロパティの型をBigDecimalにしておく
+     * 
+     */
+
+    static final String INTEGRAL_MESSAGE_ID = "org.seasar.teeda.extension.validator.TNumberLengthValidator.INTEGRAL";
+
+    static final String FRACTION_MESSAGE_ID = "org.seasar.teeda.extension.validator.TNumberLengthValidator.FRACTION";
+
+    static final String BOTH_MESSAGE_ID = "org.seasar.teeda.extension.validator.TNumberLengthValidator.BOTH";
+
+    private static final BigDecimal ZERO = new BigDecimal("0");
+
+    private int integralMin = Integer.MIN_VALUE;
+
+    private int integralMax = Integer.MAX_VALUE;
+
+    private int fractionMin = Integer.MIN_VALUE;
+
+    private int fractionMax = Integer.MAX_VALUE;
+
+    public void validate(final FacesContext context,
+            final UIComponent component, final Object value)
+            throws FacesException {
+        AssertionUtil.assertNotNull("context", context);
+        AssertionUtil.assertNotNull("component", component);
+        if (value == null) {
+            return;
+        }
+
+        final Digits digits = getDigits(context, value);
+        final boolean integralSuccess = validateIntegral(digits);
+        final boolean fractionSuccess = validateFraction(digits);
+
+        if (!integralSuccess && !fractionSuccess) {
+            final Object[] args = { value.toString(), new Integer(integralMin),
+                    new Integer(integralMax), new Integer(fractionMin),
+                    new Integer(fractionMax) };
+            final FacesMessage message = FacesMessageUtil.getMessage(context,
+                    BOTH_MESSAGE_ID, args);
+            throw new ValidatorException(message, BOTH_MESSAGE_ID, args);
+        } else if (!integralSuccess) {
+            final Object[] args = { value.toString(), new Integer(integralMin),
+                    new Integer(integralMax) };
+            final FacesMessage message = FacesMessageUtil.getMessage(context,
+                    INTEGRAL_MESSAGE_ID, args);
+            throw new ValidatorException(message, INTEGRAL_MESSAGE_ID, args);
+        } else if (!fractionSuccess) {
+            final Object[] args = { value.toString(), new Integer(fractionMin),
+                    new Integer(fractionMax) };
+            final FacesMessage message = FacesMessageUtil.getMessage(context,
+                    FRACTION_MESSAGE_ID, args);
+            throw new ValidatorException(message, FRACTION_MESSAGE_ID, args);
+        }
+    }
+
+    private boolean validateFraction(Digits digits) {
+        if (fractionMin <= fractionMax) {
+            final int fraction = digits.getFraction();
+            if (fraction < fractionMin || fractionMax < fraction) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean validateIntegral(Digits digits) {
+        if (integralMin <= integralMax) {
+            final int integral = digits.getIntegral();
+            if (integral < integralMin || integralMax < integral) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected Digits getDigits(final FacesContext context, final Object value) {
+        final Digits digits = new Digits();
+        if (value instanceof Integer) {
+            final Integer num = (Integer) value;
+            final int abs = Math.abs(num.intValue());
+            digits.setIntegral(String.valueOf(abs).length());
+        } else if (value instanceof Long) {
+            final Long num = (Long) value;
+            final long abs = Math.abs(num.longValue());
+            digits.setIntegral(String.valueOf(abs).length());
+        } else if (value instanceof BigDecimal) {
+            BigDecimal num = (BigDecimal) value;
+            if (num.compareTo(ZERO) < 0) {
+                num = num.negate();
+            }
+            final String s = num.toString();
+            final Locale locale = context.getViewRoot().getLocale();
+            final String dcimalSeparator = NumberConversionUtil
+                    .findDecimalSeparator(locale);
+            final int pos = s.indexOf(dcimalSeparator);
+            if (-1 < pos) {
+                digits.setIntegral(s.substring(0, pos).length());
+                digits.setFraction(s.substring(pos + 1).length());
+            } else {
+                digits.setIntegral(s.length());
+            }
+        }
+        return digits;
+    }
+
+    public int getFractionMax() {
+        return fractionMax;
+    }
+
+    public void setFractionMax(int fractionMax) {
+        this.fractionMax = fractionMax;
+    }
+
+    public int getFractionMin() {
+        return fractionMin;
+    }
+
+    public void setFractionMin(int fractionMin) {
+        this.fractionMin = fractionMin;
+    }
+
+    public int getIntegralMax() {
+        return integralMax;
+    }
+
+    public void setIntegralMax(int integralMax) {
+        this.integralMax = integralMax;
+    }
+
+    public int getIntegralMin() {
+        return integralMin;
+    }
+
+    public void setIntegralMin(int integralMin) {
+        this.integralMin = integralMin;
+    }
+
+    protected static class Digits {
+
+        private int integral;
+
+        private int fraction;
+
+        public int getFraction() {
+            return fraction;
+        }
+
+        public void setFraction(int fraction) {
+            this.fraction = fraction;
+        }
+
+        public int getIntegral() {
+            return integral;
+        }
+
+        public void setIntegral(int integral) {
+            this.integral = integral;
+        }
+    }
+
+}
---
+++ teeda/teeda-extension/src/test/java/org/seasar/teeda/extension/validator/TNumberLengthValidatorTest.java
@@ -0,0 +1,420 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.extension.validator;
+
+import java.math.BigDecimal;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+import org.seasar.teeda.core.mock.MockUIComponent;
+import org.seasar.teeda.core.unit.ExceptionAssert;
+import org.seasar.teeda.core.unit.TeedaTestCase;
+
+/**
+ * @author manhole
+ */
+public class TNumberLengthValidatorTest extends TeedaTestCase {
+
+    // NG:整数部が最大桁数より大きい
+    public void testValidate_IntegralGreater() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(3);
+        validator.setIntegralMin(1);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("aaa");
+        try {
+            validator.validate(getFacesContext(), component,
+                    new Integer("1234"));
+            fail();
+        } catch (final ValidatorException e) {
+            final FacesMessage facesMessage = e.getFacesMessage();
+            System.out.println(facesMessage.getSummary());
+            System.out.println(facesMessage.getDetail());
+            assertEquals(TNumberLengthValidator.INTEGRAL_MESSAGE_ID, e
+                    .getMessageId());
+            ExceptionAssert.assertMessageExist(e);
+        }
+    }
+
+    // NG:整数部が最小桁数より小さい
+    public void testValidate_IntegralLesser() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(10);
+        validator.setIntegralMin(5);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("aaa");
+        try {
+            validator.validate(getFacesContext(), component,
+                    new Integer("1234"));
+            fail();
+        } catch (final ValidatorException e) {
+            final FacesMessage facesMessage = e.getFacesMessage();
+            System.out.println(facesMessage.getSummary());
+            System.out.println(facesMessage.getDetail());
+            assertEquals(TNumberLengthValidator.INTEGRAL_MESSAGE_ID, e
+                    .getMessageId());
+            ExceptionAssert.assertMessageExist(e);
+        }
+    }
+
+    // NG:小数部が最大桁数より大きい
+    public void testValidate_FractionGreater() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setFractionMax(4);
+        validator.setFractionMin(2);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        try {
+            validator.validate(getFacesContext(), component, new BigDecimal(
+                    "123.54321"));
+            fail();
+        } catch (final ValidatorException e) {
+            final FacesMessage facesMessage = e.getFacesMessage();
+            System.out.println(facesMessage.getSummary());
+            System.out.println(facesMessage.getDetail());
+            assertEquals(TNumberLengthValidator.FRACTION_MESSAGE_ID, e
+                    .getMessageId());
+            ExceptionAssert.assertMessageExist(e);
+        }
+    }
+
+    // NG:小数部が最小桁数より小さい
+    public void testValidate_FractionLesser() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setFractionMax(4);
+        validator.setFractionMin(2);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        try {
+            validator.validate(getFacesContext(), component, new BigDecimal(
+                    "123.54321"));
+            fail();
+        } catch (final ValidatorException e) {
+            final FacesMessage facesMessage = e.getFacesMessage();
+            System.out.println(facesMessage.getSummary());
+            System.out.println(facesMessage.getDetail());
+            assertEquals(TNumberLengthValidator.FRACTION_MESSAGE_ID, e
+                    .getMessageId());
+            ExceptionAssert.assertMessageExist(e);
+        }
+    }
+
+    // NG:整数部は範囲内だが小数部が最小桁数より小さい
+    public void testValidate_IntegralOkAndFractionLesser() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(7);
+        validator.setIntegralMin(5);
+        validator.setFractionMax(4);
+        validator.setFractionMin(2);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        try {
+            validator.validate(getFacesContext(), component, new BigDecimal(
+                    "12345.1"));
+            fail();
+        } catch (final ValidatorException e) {
+            final FacesMessage facesMessage = e.getFacesMessage();
+            System.out.println(facesMessage.getSummary());
+            System.out.println(facesMessage.getDetail());
+            assertEquals(TNumberLengthValidator.FRACTION_MESSAGE_ID, e
+                    .getMessageId());
+            ExceptionAssert.assertMessageExist(e);
+        }
+    }
+
+    // NG:小数部は範囲内だが整数部が最小桁数より小さい
+    public void testValidate_IntegralLesserAndFractionOk() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(7);
+        validator.setIntegralMin(5);
+        validator.setFractionMax(4);
+        validator.setFractionMin(2);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        try {
+            validator.validate(getFacesContext(), component, new BigDecimal(
+                    "1234.12"));
+            fail();
+        } catch (final ValidatorException e) {
+            final FacesMessage facesMessage = e.getFacesMessage();
+            System.out.println(facesMessage.getSummary());
+            System.out.println(facesMessage.getDetail());
+            assertEquals(TNumberLengthValidator.INTEGRAL_MESSAGE_ID, e
+                    .getMessageId());
+            ExceptionAssert.assertMessageExist(e);
+        }
+    }
+
+    // NG:整数部も小数部も最小桁数より小さい
+    public void testValidate_BothIntegralAndFractionLesser() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(7);
+        validator.setIntegralMin(5);
+        validator.setFractionMax(4);
+        validator.setFractionMin(3);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        try {
+            validator.validate(getFacesContext(), component, new BigDecimal(
+                    "123.4"));
+            fail();
+        } catch (final ValidatorException e) {
+            final FacesMessage facesMessage = e.getFacesMessage();
+            System.out.println(facesMessage.getSummary());
+            System.out.println(facesMessage.getDetail());
+            assertEquals(TNumberLengthValidator.BOTH_MESSAGE_ID, e
+                    .getMessageId());
+            ExceptionAssert.assertMessageExist(e);
+        }
+    }
+
+    // OK:整数部だけ入力されていて範囲内(最小値 < x < 最大値)
+    public void testValidate_IntegralInRange() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(5);
+        validator.setIntegralMin(2);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        validator.validate(getFacesContext(), component, new BigDecimal("123"));
+    }
+
+    // OK:整数部だけ入力されていて範囲内(最小値 == x)
+    public void testValidate_IntegralMin() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(5);
+        validator.setIntegralMin(2);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        validator.validate(getFacesContext(), component, new BigDecimal("12"));
+    }
+
+    // OK:整数部だけ入力されていて範囲内(x == 最大値)
+    public void testValidate_IntegralMax() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(5);
+        validator.setIntegralMin(2);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        validator.validate(getFacesContext(), component,
+                new BigDecimal("12345"));
+    }
+
+    // OK:小数部だけ入力されていて範囲内(最小値 < x < 最大値)
+    public void testValidate_FractionInRange() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setFractionMax(5);
+        validator.setFractionMin(3);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        validator.validate(getFacesContext(), component, new BigDecimal(
+                "0.1234"));
+    }
+
+    // OK:小数部だけ入力されていて範囲内(最小値 == x)
+    public void testValidate_FractionMin() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setFractionMax(5);
+        validator.setFractionMin(3);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        validator.validate(getFacesContext(), component,
+                new BigDecimal("0.123"));
+    }
+
+    // OK:小数部だけ入力されていて範囲内(x == 最大値)
+    public void testValidate_FractionMax() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setFractionMax(5);
+        validator.setFractionMin(3);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        validator.validate(getFacesContext(), component, new BigDecimal(
+                "0.12345"));
+    }
+
+    // OK:整数部も小数部も範囲内 testValidate_BothIntegralAndFractionLesser
+    public void testValidate_BothIntegralAndFractionInRange() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(10);
+        validator.setFractionMax(4);
+        validator.setFractionMin(2);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("a");
+        validator.validate(getFacesContext(), component, new BigDecimal(
+                "1234567.123"));
+    }
+
+    // OK:入力値がnull
+    public void testValidate_NullValue() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        validator.setIntegralMax(3);
+        validator.setIntegralMin(1);
+        final MockUIComponent component = new MockUIComponent();
+        component.setId("aaa");
+
+        validator.validate(getFacesContext(), component, null);
+    }
+
+    public void testGetDigits_Integer() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        final FacesContext context = getFacesContext();
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Integer(123));
+            assertEquals(3, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Integer(1234567890));
+            assertEquals(10, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Integer(0));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Integer(1));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Integer(-1124));
+            assertEquals(4, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+    }
+
+    public void testGetDigits_Long() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        final FacesContext context = getFacesContext();
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Long(123));
+            assertEquals(3, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Long(1234567890123456789L));
+            assertEquals(19, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Long(0));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Long(1));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new Long(-1234));
+            assertEquals(4, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+    }
+
+    public void testGetDigits_BigDecimal_Integral() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        final FacesContext context = getFacesContext();
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("123"));
+            assertEquals(3, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("1234567890123456789"));
+            assertEquals(19, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("123456789012345678901234567890"));
+            assertEquals(30, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("0"));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("1"));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("-123456"));
+            assertEquals(6, digits.getIntegral());
+            assertEquals(0, digits.getFraction());
+        }
+    }
+
+    public void testGetDigits_BigDecimal_Fraction() throws Exception {
+        final TNumberLengthValidator validator = new TNumberLengthValidator();
+        final FacesContext context = getFacesContext();
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("0.1"));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(1, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("0.1234567890123456789"));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(19, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context,
+                    new BigDecimal("10.123456789012345678901234567890"));
+            assertEquals(2, digits.getIntegral());
+            assertEquals(30, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("0.0"));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(1, digits.getFraction());
+        }
+        {
+            final TNumberLengthValidator.Digits digits = validator.getDigits(
+                    context, new BigDecimal("-1.123456"));
+            assertEquals(1, digits.getIntegral());
+            assertEquals(6, digits.getFraction());
+        }
+    }
+
+}
