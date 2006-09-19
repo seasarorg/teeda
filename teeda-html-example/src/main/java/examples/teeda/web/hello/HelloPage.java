eae236b4a2334fff4903525e0fe7c4e6ea59c15e
Yasuo Higa <higayasuo@gmail.com>
2006-09-19 02:02:09 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1919 e0972215-f904-0410-97d3-99f68b247671

--- teeda-html-example/src/main/java/examples/teeda/web/hello/HelloPage.java
+++ teeda-html-example/src/main/java/examples/teeda/web/hello/HelloPage.java
@@ -1,12 +1,8 @@
 package examples.teeda.web.hello;
 
-import javax.faces.context.FacesContext;
-
 public class HelloPage {
 
 	private String name = "Seasar2";
-
-	private FacesContext facesContext;
 
 	public String getName() {
 		return name;
@@ -23,15 +19,6 @@ public class HelloPage {
 
 	public String prerender() {
 		System.out.println("prerender");
-		System.out.println("facesContext:" + facesContext);
 		return null;
-	}
-
-	public FacesContext getFacesContext() {
-		return facesContext;
-	}
-
-	public void setFacesContext(FacesContext facesContext) {
-		this.facesContext = facesContext;
 	}
 }
