2f4a63f41dda094cca0e9911cb78dbe17a94ee67
Yasuo Higa <higayasuo@gmail.com>
2006-09-20 06:09:41 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1939 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/textarea/TextareaPage.java
@@ -0,0 +1,15 @@
+package examples.teeda.web.textarea;
+
+public class TextareaPage {
+
+	private String aaa;
+
+	public String getAaa() {
+		return aaa;
+	}
+
+	public void setAaa(String aaa) {
+		this.aaa = aaa;
+	}
+
+}
