5b0b45ccaa43bfcb9beee37b185396aaa3610e72
manhole <manholex@gmail.com>
2006-09-24 23:53:07 +0000

    https://www.seasar.org/issues/browse/TEEDA-112 を含めたForeachのテストを書いた。
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@1984 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/teeda-html-test/src/test/java/org/seasar/teeda/it/render/ForeachTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it.render;
+
+import junit.framework.Test;
+
+import org.seasar.teeda.unit.web.TeedaWebTestCase;
+import org.seasar.teeda.unit.web.TeedaWebTester;
+
+/**
+ * @author manhole
+ */
+public class ForeachTest extends TeedaWebTestCase {
+
+    public static Test suite() throws Exception {
+        return setUpTest(ForeachTest.class);
+    }
+
+    public void testRender() throws Exception {
+        // ## Arrange ##
+        TeedaWebTester tester = new TeedaWebTester();
+        tester.getTestContext().setBaseUrl(getBaseUrl());
+        tester.beginAt("view/foreach/foreach.html");
+        tester.dumpHtml();
+        tester.assertTableEquals("foreachTable",
+            new String[][] { { "1", "aa1", "bb1" }, { "2", "aa2", "bb2" },
+                { "3", "aa3", "bb3" } });
+        tester.clickButton("doSomething");
+
+        // --------
+
+        tester.dumpHtml();
+        tester.assertTableEquals("foreachTable",
+            new String[][] { { "1", "aa1", "bb1" }, { "2", "aa2", "bb2" },
+                { "3", "aa3", "bb3" } });
+    }
+
+}
