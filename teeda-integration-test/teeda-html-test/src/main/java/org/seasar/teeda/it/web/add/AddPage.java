5a2e801758271aa0f818708cfc95546a6b8c8634
shot6 <shinpei.ohtani@gmail.com>
2006-09-27 11:16:02 +0000

    add simple AddTest.
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2018 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/teeda-html-test/src/main/java/org/seasar/teeda/it/web/add/AddPage.java
@@ -0,0 +1,43 @@
+package org.seasar.teeda.it.web.add;
+
+public class AddPage {
+
+	public static final String arg1_TGreaterThanConstantValidator = null;
+
+	public static final String arg2_TGreaterThanConstantValidator = null;
+
+	private int arg1;
+
+	private int arg2;
+
+	private int result;
+
+	public int getArg1() {
+		return arg1;
+	}
+
+	public void setArg1(int arg1) {
+		this.arg1 = arg1;
+	}
+
+	public int getArg2() {
+		return arg2;
+	}
+
+	public void setArg2(int arg2) {
+		this.arg2 = arg2;
+	}
+
+	public int getResult() {
+		return result;
+	}
+
+	public void setResult(int result) {
+		this.result = result;
+	}
+
+	public String doCalculate() {
+		result = arg1 + arg2;
+		return null;
+	}
+}
