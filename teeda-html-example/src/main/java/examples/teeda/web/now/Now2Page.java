6559109c04423615b870aa5daa7aa0307e7a31e4
shot6 <shinpei.ohtani@gmail.com>
2006-09-28 08:16:54 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2022 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/now/Now2Page.java
@@ -0,0 +1,39 @@
+package examples.teeda.web.now;
+
+import java.util.Date;
+
+public class Now2Page {
+
+	public static final String now_dateTimeConverter = "pattern='yyyy/MM'";
+	
+	public static final String now_TRequiredValidator = null;
+	
+	private String nowPattern = "yyyy/MM";
+	
+	private Date now;
+
+	public Date getNow() {
+		return now;
+	}
+
+	public void setNow(Date now) {
+		this.now = now;
+	}
+
+	public String initialize() {
+		return null;
+	}
+
+	public String doCalculate() {
+		return null;
+	}
+
+	public String getNowPattern() {
+		return nowPattern;
+	}
+
+	public void setNowPattern(String nowPattern) {
+		this.nowPattern = nowPattern;
+	}
+
+}
