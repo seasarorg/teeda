89b828b5a0cd86680d35d27f63d980b56fd1e03e
shot6 <shinpei.ohtani@gmail.com>
2006-10-04 01:12:27 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2052 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/add/AddHoge2Page.java
@@ -0,0 +1,40 @@
+/**
+ *
+ */
+package examples.teeda.web.add;
+
+/**
+ * @author shot
+ * 
+ */
+public class AddHoge2Page {
+
+	private String a;
+
+	private String b;
+
+	public String initilize() {
+		return null;
+	}
+
+	public String prerender() {
+		return null;
+	}
+
+	public String getA() {
+		return a;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+
+}
---
+++ teeda-html-example/src/main/java/examples/teeda/web/add/AddHogePage.java
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+package examples.teeda.web.add;
+
+/**
+ * @author shot
+ * 
+ */
+public class AddHogePage {
+
+	private String a;
+
+	private String b;
+
+	private boolean show;
+
+	public String doRead() {
+		setShow(true);
+		return null;
+	}
+
+	public String getA() {
+		return a;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public boolean isShow() {
+		return show;
+	}
+
+	public void setA(String a) {
+		this.a = a;
+	}
+
+	public void setB(String b) {
+		this.b = b;
+	}
+
+	public void setShow(boolean show) {
+		this.show = show;
+	}
+}
