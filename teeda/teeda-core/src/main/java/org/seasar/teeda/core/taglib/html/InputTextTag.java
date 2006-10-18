f7662e0f0cc143e3d45cf2f2002d08ff9ca6e3a6
shot6 <shinpei.ohtani@gmail.com>
2006-10-18 11:25:19 +0000

    m(_          _)m
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2157 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/taglib/html/InputTextTag.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/taglib/html/InputTextTag.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -34,6 +34,14 @@ public class InputTextTag extends InputTagBase {
 
     public String getRendererType() {
         return "javax.faces.Text";
+    }
+
+    public String getAutocomplete() {
+        return autocomplete;
+    }
+
+    public void setAutocomplete(String autocomplete) {
+        this.autocomplete = autocomplete;
     }
 
     protected void setProperties(UIComponent component) {
