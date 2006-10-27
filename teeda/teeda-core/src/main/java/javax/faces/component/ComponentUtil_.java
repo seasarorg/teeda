be348be472ddc7439fa41c24de1796ba5ea2097f
shot6 <shinpei.ohtani@gmail.com>
2006-10-27 12:33:31 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2226 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/javax/faces/component/ComponentUtil_.java
+++ teeda/teeda-core/src/main/java/javax/faces/component/ComponentUtil_.java
@@ -94,7 +94,7 @@ public class ComponentUtil_ {
     public static Locale getLocale(FacesContext context) {
         AssertionUtil.assertNotNull("context", context);
         final UIViewRoot viewRoot = context.getViewRoot();
-        return (viewRoot != null) ? viewRoot.getLocale() : null;
+        return (viewRoot != null) ? viewRoot.getLocale() : Locale.getDefault();
     }
 
     public static boolean isLocaleShort(String locale) {
