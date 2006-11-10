6673c37787877e5d9a47f80853f21a1264280fbb
shot6 <shinpei.ohtani@gmail.com>
2006-11-10 11:47:59 +0000

    add condition test.
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2333 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/teeda-html-test/src/test/java/org/seasar/teeda/it/render/ConditionTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it.render;
+
+import junit.framework.Test;
+
+import org.seasar.teeda.unit.web.TeedaWebTestCase;
+import org.seasar.teeda.unit.web.TeedaWebTester;
+
+/**
+ * @author shot
+ */
+public class ConditionTest extends TeedaWebTestCase {
+
+	public static Test suite() throws Exception {
+		return setUpTest(ConditionTest.class);
+	}
+
+	public void testConditionRestoreWhenValidationError() throws Exception {
+		// ## Arrange ##
+		TeedaWebTester tester = new TeedaWebTester();
+		tester.getTestContext().setBaseUrl(getBaseUrl());
+
+		// ## Act ##
+		tester.beginAt("view/condition/condition.html");
+		tester.dumpHtml();
+
+		tester.assertElementPresent("isAaa");
+		tester.assertElementNotPresent("isNotAaa");
+		tester.setTextById("hoge", "1");
+
+		tester.submit("form:doHoge-1");
+
+		// ## Assert ##
+		tester.assertElementPresent("isAaa");
+		tester.assertElementNotPresent("isNotAaa");
+	}
+
+}
