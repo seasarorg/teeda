6080214a0add0514193758b995b3b9498cf7578f
Yasuo Higa <higayasuo@gmail.com>
2007-02-08 07:02:45 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2770 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/prefix/PrefixPage.java
@@ -0,0 +1,16 @@
+package examples.teeda.web.prefix;
+
+public class PrefixPage {
+
+	public String initialize() {
+		return null;
+	}
+
+	public String prerender() {
+		return null;
+	}
+
+	public String doAction() {
+		return "prefix2";
+	}
+}
