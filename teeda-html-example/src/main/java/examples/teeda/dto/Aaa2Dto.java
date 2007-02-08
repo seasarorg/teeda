ea2144d33158f2cc6de09ec648f35e56b5c804b3
shot6 <shinpei.ohtani@gmail.com>
2007-02-08 01:10:29 +0000

    errorページで無限REDIRECTが抑止されているかチェックするサンプルをコミット。 dtoがTeedaで正しくSmartDeployできているかのサンプルをコミット。
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2762 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/dto/Aaa2Dto.java
@@ -0,0 +1,19 @@
+package examples.teeda.dto;
+
+public class Aaa2Dto {
+
+	private String bbb;
+
+	public Aaa2Dto() {
+		System.out.println("aaa");
+	}
+
+	public String getBbb() {
+		return bbb;
+	}
+
+	public void setBbb(String bbb) {
+		this.bbb = bbb;
+	}
+
+}
