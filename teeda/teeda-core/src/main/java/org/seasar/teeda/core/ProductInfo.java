04a671c4a40ea4bac8ef740c6044afdccfb3d36a
shot6 <shinpei.ohtani@gmail.com>
2007-02-12 03:17:58 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2783 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/ProductInfo.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/ProductInfo.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -27,7 +27,7 @@ public class ProductInfo {
 
     private static final String LOCAL_VERSION = "4";
 
-    private static boolean isSnapshot = true;
+    private static boolean isSnapshot = false;
 
     public static String getProductName() {
         return "Teeda";
