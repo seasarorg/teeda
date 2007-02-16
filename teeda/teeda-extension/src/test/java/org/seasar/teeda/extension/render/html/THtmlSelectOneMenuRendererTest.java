90d93b89853b0e5805fbc44b3069adc260c5fc96
shot6 <shinpei.ohtani@gmail.com>
2007-02-16 07:05:47 +0000

    TEEDA-250.
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2797 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-extension/src/test/java/org/seasar/teeda/extension/render/html/THtmlSelectOneMenuRendererTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.extension.render.html;
+
+import javax.faces.component.UISelectItem;
+import javax.faces.context.FacesContext;
+import javax.faces.render.Renderer;
+import javax.faces.render.RendererTest;
+
+import org.seasar.teeda.extension.component.html.THtmlSelectOneMenu;
+
+/**
+ * @author shot
+ */
+public class THtmlSelectOneMenuRendererTest extends RendererTest {
+
+    private THtmlSelectOneMenuRenderer renderer;
+
+    private MockTHtmlSelectOneMenu htmlSelectOneMenu;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        renderer = createTHtmlSelectOneMenuRenderer();
+        htmlSelectOneMenu = new MockTHtmlSelectOneMenu();
+        htmlSelectOneMenu.setRenderer(renderer);
+
+        // MockHtmlSelectOneMenuのプロパティ
+        renderer.addIgnoreAttributeName("setSubmittedValueCalls");
+    }
+
+    public void testEncode_WithPageName() throws Exception {
+        // ## Arrange ##
+        {
+            UISelectItem selectItem = new UISelectItem();
+            selectItem.setItemValue("v");
+            selectItem.setItemLabel("l");
+            htmlSelectOneMenu.getChildren().add(selectItem);
+        }
+        htmlSelectOneMenu.setPageName("hogePage");
+
+        // ## Act ##
+        encodeByRenderer(renderer, htmlSelectOneMenu);
+
+        // ## Assert ##
+        assertEquals("<select name=\"_id0\" size=\"1\">"
+                + "<option value=\"v\">l</option>" + "</select>",
+                getResponseText());
+    }
+
+    public void testEncode_WithLabelName() throws Exception {
+        // ## Arrange ##
+        {
+            UISelectItem selectItem = new UISelectItem();
+            selectItem.setItemValue("v");
+            selectItem.setItemLabel("l");
+            htmlSelectOneMenu.getChildren().add(selectItem);
+        }
+        htmlSelectOneMenu.setLabelName("aaaLabel");
+
+        // ## Act ##
+        encodeByRenderer(renderer, htmlSelectOneMenu);
+
+        // ## Assert ##
+        assertEquals("<select name=\"_id0\" size=\"1\">"
+                + "<option value=\"v\">l</option>" + "</select>",
+                getResponseText());
+    }
+
+    private THtmlSelectOneMenuRenderer createTHtmlSelectOneMenuRenderer() {
+        return (THtmlSelectOneMenuRenderer) createRenderer();
+    }
+
+    protected Renderer createRenderer() {
+        THtmlSelectOneMenuRenderer renderer = new THtmlSelectOneMenuRenderer();
+        renderer.setComponentIdLookupStrategy(getComponentIdLookupStrategy());
+        return renderer;
+    }
+
+    public static class MockTHtmlSelectOneMenu extends THtmlSelectOneMenu {
+
+        private Renderer renderer_;
+
+        private String clientId_;
+
+        private int setSubmittedValueCalls_;
+
+        public void setRenderer(Renderer renderer) {
+            renderer_ = renderer;
+        }
+
+        protected Renderer getRenderer(FacesContext context) {
+            if (renderer_ != null) {
+                return renderer_;
+            }
+            return super.getRenderer(context);
+        }
+
+        public String getClientId(FacesContext context) {
+            if (clientId_ != null) {
+                return clientId_;
+            }
+            return super.getClientId(context);
+        }
+
+        public void setClientId(String clientId) {
+            clientId_ = clientId;
+        }
+
+        public void setSubmittedValue(Object submittedValue) {
+            setSubmittedValueCalls_++;
+            super.setSubmittedValue(submittedValue);
+        }
+
+        public int getSetSubmittedValueCalls() {
+            return setSubmittedValueCalls_;
+        }
+
+    }
+
+}
