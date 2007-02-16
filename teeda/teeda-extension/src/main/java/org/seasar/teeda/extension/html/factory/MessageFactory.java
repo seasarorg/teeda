b4a439a2550efea6600d8bb788201224e96ecc33
shot6 <shinpei.ohtani@gmail.com>
2007-02-16 07:56:51 +0000

    TEEDA-252
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2799 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-extension/src/main/java/org/seasar/teeda/extension/html/factory/MessageFactory.java
+++ teeda/teeda-extension/src/main/java/org/seasar/teeda/extension/html/factory/MessageFactory.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -25,7 +25,7 @@ import org.seasar.teeda.extension.html.PageDesc;
 
 /**
  * @author higa
- *
+ * @author shot
  */
 public class MessageFactory extends AbstractElementProcessorFactory {
 
@@ -59,6 +59,10 @@ public class MessageFactory extends AbstractElementProcessorFactory {
 
     protected String getUri() {
         return ExtensionConstants.TEEDA_EXTENSION_URI;
+    }
+
+    public boolean isLeaf() {
+        return true;
     }
 
 }
