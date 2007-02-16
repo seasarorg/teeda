5771e9dbcbde5f7cb0794d60a718732beb6a0df0
shot6 <shinpei.ohtani@gmail.com>
2007-02-16 08:34:43 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2808 e0972215-f904-0410-97d3-99f68b247671

--- teeda-integration-test/teeda-html-test/src/test/java/org/seasar/teeda/it/render/ErrorTest.java
+++ teeda-integration-test/teeda-html-test/src/test/java/org/seasar/teeda/it/render/ErrorTest.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -43,7 +43,7 @@ public class ErrorTest extends TeedaWebTestCase {
 		assertTrue(tester.getCurrentUri().endsWith("errorResult.html"));
 	}
 
-	public void testErrorOnInitialize() throws Exception {
+	 public void testErrorOnInitialize() throws Exception {
 		// ## Arrange ##
 		TeedaWebTester tester = new TeedaWebTester();
 
