feb06287d4d57d1a5061578f11175e3be6be37e3
Yasuo Higa <higayasuo@gmail.com>
2007-02-23 07:18:52 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2850 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-extension/src/main/java/org/seasar/teeda/extension/html/factory/ScopeOutputTextFactory.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.extension.html.factory;
+
+import java.util.Map;
+
+import org.seasar.framework.util.StringUtil;
+import org.seasar.teeda.core.JsfConstants;
+import org.seasar.teeda.core.util.BindingUtil;
+import org.seasar.teeda.extension.html.ActionDesc;
+import org.seasar.teeda.extension.html.ElementNode;
+import org.seasar.teeda.extension.html.PageDesc;
+
+/**
+ * @author higa
+ * 
+ */
+public class ScopeOutputTextFactory extends AbstractElementProcessorFactory {
+
+    private static final String TAG_NAME = "outputText";
+
+    public ScopeOutputTextFactory() {
+    }
+
+    public boolean isMatch(ElementNode elementNode, PageDesc pageDesc,
+            ActionDesc actionDesc) {
+        if (!JsfConstants.SPAN_ELEM.equalsIgnoreCase(elementNode.getTagName())) {
+            return false;
+        }
+        String id = elementNode.getId();
+        if (id == null) {
+            return false;
+        }
+        return id.indexOf("Scope_") >= 0;
+    }
+
+    public boolean isLeaf() {
+        return true;
+    }
+
+    protected void customizeProperties(Map properties, ElementNode elementNode,
+            PageDesc pageDesc, ActionDesc actionDesc) {
+        super
+                .customizeProperties(properties, elementNode, pageDesc,
+                        actionDesc);
+        properties.put(JsfConstants.VALUE_ATTR, BindingUtil
+                .getExpression(StringUtil
+                        .replace(elementNode.getId(), "_", ".")));
+    }
+
+    protected String getTagName() {
+        return TAG_NAME;
+    }
+
+    protected String getUri() {
+        return JsfConstants.JSF_HTML_URI;
+    }
+}
---
+++ teeda/teeda-extension/src/test/java/org/seasar/teeda/extension/html/factory/ScopeOutputTextFactoryTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.extension.html.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.seasar.teeda.core.JsfConstants;
+import org.seasar.teeda.core.taglib.html.OutputTextTag;
+import org.seasar.teeda.extension.html.ElementNode;
+import org.seasar.teeda.extension.html.ElementProcessor;
+
+/**
+ * @author higa
+ */
+public class ScopeOutputTextFactoryTest extends ElementProcessorFactoryTestCase {
+
+    protected AbstractElementProcessorFactory createFactory() {
+        return new ScopeOutputTextFactory();
+    }
+
+    protected void registerTagElements() {
+        registerTagElement(JsfConstants.JSF_HTML_URI, "outputText",
+                OutputTextTag.class);
+    }
+
+    public void testIsMatch() throws Exception {
+        Map properties = new HashMap();
+        properties.put("id", "requestScope_aaa");
+        ElementNode elementNode = createElementNode("span", properties);
+        assertTrue(factory.isMatch(elementNode, null, null));
+
+        ElementNode elementNode2 = createElementNode("foo", properties);
+        assertFalse(factory.isMatch(elementNode2, null, null));
+
+        properties.put("id", "hoge");
+        ElementNode elementNode3 = createElementNode("span", properties);
+        assertFalse(factory.isMatch(elementNode3, null, null));
+    }
+
+    public void testCreateProcessor() throws Exception {
+        // ## Arrange ##
+        Map properties = new HashMap();
+        properties.put("id", "requestScope_aaa");
+        ElementNode elementNode = createElementNode("span", properties);
+
+        // ## Act ##
+        ElementProcessor processor = factory.createProcessor(elementNode, null,
+                null);
+        // ## Assert ##
+        assertNotNull(processor);
+        assertEquals(OutputTextTag.class, processor.getTagClass());
+        assertEquals("#{requestScope.aaa}", processor.getProperty("value"));
+    }
+}
