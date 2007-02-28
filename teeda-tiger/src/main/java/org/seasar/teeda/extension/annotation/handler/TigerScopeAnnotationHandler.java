464a133ea89f163e4631dcb5f8405fcd80b64493
shot6 <shinpei.ohtani@gmail.com>
2007-02-28 07:01:45 +0000

    SubApplicationScope, PageScope.
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2860 e0972215-f904-0410-97d3-99f68b247671

--- teeda-tiger/src/main/java/org/seasar/teeda/extension/annotation/handler/TigerScopeAnnotationHandler.java
+++ teeda-tiger/src/main/java/org/seasar/teeda/extension/annotation/handler/TigerScopeAnnotationHandler.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -20,11 +20,13 @@ import java.util.Map;
 
 import org.seasar.framework.beans.BeanDesc;
 import org.seasar.teeda.extension.ExtensionConstants;
+import org.seasar.teeda.extension.annotation.scope.PageScope;
 import org.seasar.teeda.extension.annotation.scope.RedirectScope;
+import org.seasar.teeda.extension.annotation.scope.SubapplicationScope;
 
 /**
  * @author higa
- * 
+ * @author shot
  */
 public class TigerScopeAnnotationHandler extends ConstantScopeAnnotationHandler {
 
@@ -33,13 +35,34 @@ public class TigerScopeAnnotationHandler extends ConstantScopeAnnotationHandler 
 	public void setupPropertyScopes(BeanDesc beanDesc, Map scopes) {
 		for (int i = 0; i < beanDesc.getFieldSize(); ++i) {
 			Field field = beanDesc.getField(i);
-			RedirectScope redirectScope = field
-					.getAnnotation(RedirectScope.class);
-			if (redirectScope == null) {
-				continue;
-			}
-			scopes.put(field.getName(), ExtensionConstants.REDIRECT_SCOPE);
+			handlePageScope(field, scopes);
+			handleRedirectScope(field, scopes);
+			handleSubapplicationScope(field, scopes);
 		}
 		super.setupPropertyScopes(beanDesc, scopes);
 	}
+
+	protected void handlePageScope(Field field, Map<String, Integer> scopes) {
+		PageScope pageScope = field.getAnnotation(PageScope.class);
+		if (pageScope != null) {
+			scopes.put(field.getName(), ExtensionConstants.PAGE_SCOPE);
+		}
+	}
+
+	protected void handleRedirectScope(Field field, Map<String, Integer> scopes) {
+		RedirectScope redirectScope = field.getAnnotation(RedirectScope.class);
+		if (redirectScope != null) {
+			scopes.put(field.getName(), ExtensionConstants.REDIRECT_SCOPE);
+		}
+	}
+
+	protected void handleSubapplicationScope(Field field,
+			Map<String, Integer> scopes) {
+		SubapplicationScope subapplicationScope = field
+				.getAnnotation(SubapplicationScope.class);
+		if (subapplicationScope != null) {
+			scopes.put(field.getName(), ExtensionConstants.SUBAPP_SCOPE);
+		}
+	}
+
 }
--- teeda-tiger/src/test/java/org/seasar/teeda/extension/annotation/handler/TigerScopeAnnotationHandlerTest.java
+++ teeda-tiger/src/test/java/org/seasar/teeda/extension/annotation/handler/TigerScopeAnnotationHandlerTest.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -19,7 +19,9 @@ import java.util.Map;
 
 import org.seasar.teeda.core.unit.TeedaTestCase;
 import org.seasar.teeda.extension.ExtensionConstants;
+import org.seasar.teeda.extension.annotation.scope.PageScope;
 import org.seasar.teeda.extension.annotation.scope.RedirectScope;
+import org.seasar.teeda.extension.annotation.scope.SubapplicationScope;
 
 public class TigerScopeAnnotationHandlerTest extends TeedaTestCase {
 
@@ -30,8 +32,10 @@ public class TigerScopeAnnotationHandlerTest extends TeedaTestCase {
 	public void testGetPropertyScopes() throws Exception {
 		TigerScopeAnnotationHandler handler = new TigerScopeAnnotationHandler();
 		Map m = handler.getPropertyScopes("hogeBean2");
-		assertEquals(1, m.size());
+		assertEquals(3, m.size());
 		assertEquals(ExtensionConstants.REDIRECT_SCOPE, m.get("aaa"));
+		assertEquals(ExtensionConstants.SUBAPP_SCOPE, m.get("bbb"));
+		assertEquals(ExtensionConstants.PAGE_SCOPE, m.get("ccc"));
 	}
 
 	public static class HogeBean {
@@ -39,20 +43,36 @@ public class TigerScopeAnnotationHandlerTest extends TeedaTestCase {
 		@RedirectScope
 		private String aaa;
 
-		/**
-		 * @return Returns the aaa.
-		 */
+		@SubapplicationScope
+		private Integer bbb;
+
+		@PageScope
+		private int ccc;
+
 		public String getAaa() {
 			return aaa;
 		}
 
-		/**
-		 * @param aaa
-		 *            The aaa to set.
-		 */
 		public void setAaa(String aaa) {
 			this.aaa = aaa;
 		}
+
+		public Integer getBbb() {
+			return bbb;
+		}
+
+		public int getCcc() {
+			return ccc;
+		}
+
+		public void setBbb(Integer bbb) {
+			this.bbb = bbb;
+		}
+
+		public void setCcc(int ccc) {
+			this.ccc = ccc;
+		}
+
 	}
 
 	public static class HogeBean2 extends HogeBean {
