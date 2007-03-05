845e442a4f5d198788f7def20fae823aeaef9b85
shot6 <shinpei.ohtani@gmail.com>
2007-03-05 07:49:53 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2911 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/render/AbstractRenderer.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/render/AbstractRenderer.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -144,6 +144,7 @@ public abstract class AbstractRenderer extends Renderer {
         }
     }
 
+    //TODO performance tuning needed.
     protected Map getAllAttributesAndProperties(final UIComponent component,
             final IgnoreAttribute ignore) {
         final Map map = new HashMap();
