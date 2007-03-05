8c361a67f1295cf35d545d583c6600e5a7e6f9aa
shot6 <shinpei.ohtani@gmail.com>
2007-03-05 04:17:08 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2910 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/teeda-html-test/src/main/java/org/seasar/teeda/it/web/link/LinkResultPage.java
@@ -0,0 +1,56 @@
+package org.seasar.teeda.it.web.link;
+
+import java.util.Date;
+
+public class LinkResultPage {
+
+	private Integer arg1;
+
+	private Integer arg2;
+
+	private Integer arg3;
+
+	private Date date;
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Integer getArg3() {
+		return arg3;
+	}
+
+	public void setArg3(Integer arg3) {
+		this.arg3 = arg3;
+	}
+
+	public Integer getArg1() {
+		return arg1;
+	}
+
+	public void setArg1(Integer arg1) {
+		this.arg1 = arg1;
+	}
+
+	public Integer getArg2() {
+		return arg2;
+	}
+
+	public void setArg2(Integer arg2) {
+		this.arg2 = arg2;
+	}
+
+	public String initialize() {
+		System.out.println(date);
+		return null;
+	}
+
+	public String prerender() {
+		return null;
+	}
+
+}
---
+++ teeda-integration-test/teeda-html-test/src/test/java/org/seasar/teeda/it/render/LinkTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it.render;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import junit.framework.Test;
+
+import org.seasar.teeda.unit.web.TeedaWebTestCase;
+import org.seasar.teeda.unit.web.TeedaWebTester;
+
+/**
+ * @author shot
+ */
+public class LinkTest extends TeedaWebTestCase {
+
+	public static Test suite() throws Exception {
+		return setUpTest(LinkTest.class);
+	}
+
+	public void testLinkSimple() throws Exception {
+		// ## Arrange ##
+		TeedaWebTester tester = new TeedaWebTester();
+
+		// ## Act ##
+		tester.beginAt(getBaseUrl(), "view/link/link.html");
+		tester.dumpHtml();
+		tester.clickLinkById("goLinkResult");
+
+		// ## Assert ##
+		assertTrue(tester.getCurrentUri().indexOf("view/link/linkResult.html") >= 0);
+		String str = new SimpleDateFormat("yyyy/MM/dd").format(new Date());
+		tester.assertTextEqualsById("date", str);
+		tester.assertTextEqualsById("arg1", "1111");
+		tester.assertTextEqualsById("arg2", "2222");
+		tester.assertTextEqualsById("arg3", "3333");
+	}
+
+	public void testLinkAndBackByJump() throws Exception {
+		// ## Arrange ##
+		TeedaWebTester tester = new TeedaWebTester();
+
+		// ## Act ##
+		tester.beginAt(getBaseUrl(), "view/link/link.html");
+		tester.dumpHtml();
+		tester.clickLinkById("goLinkResult");
+
+		// ## Assert ##
+		assertTrue(tester.getCurrentUri().indexOf("view/link/linkResult.html") >= 0);
+
+		tester.dumpHtml();
+		tester.submitById("jumpLink");
+
+		assertTrue(tester.getCurrentUri().indexOf("view/link/link.html") >= 0);
+	}
+
+}
