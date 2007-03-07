a6e9e6258d2640f8c0e3ba90d2086d69523ce24c
Yasuo Higa <higayasuo@gmail.com>
2007-03-07 04:04:22 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2924 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/webapp/WEB-INF/bin/runHsqldb.bat
@@ -0,0 +1,2 @@
+cd ..\..\..\..\main\resources\data
+@java -classpath ../../webapp/WEB-INF/lib/hsqldb-1.8.0.1.jar org.hsqldb.Server
---
+++ teeda-html-example/src/main/webapp/WEB-INF/bin/runHsqldbManager.bat
@@ -0,0 +1,2 @@
+cd ..\..\..\..\main\resources\data
+@java -classpath ../../webapp/WEB-INF/lib/hsqldb-1.8.0.1.jar org.hsqldb.util.DatabaseManager %1 %2 %3 %4 %5 %6 %7 %8 %9
