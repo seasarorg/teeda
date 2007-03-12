59a42dd89d77a8471fd4fe46715b23261de26bb1
shot6 <shinpei.ohtani@gmail.com>
2007-03-12 06:28:24 +0000

    例外にRootCauseを含めた。
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@2950 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-ajax/src/main/java/org/seasar/teeda/ajax/AjaxServlet.java
+++ teeda/teeda-ajax/src/main/java/org/seasar/teeda/ajax/AjaxServlet.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -115,7 +115,7 @@ public class AjaxServlet extends HttpServlet {
         } catch (Exception e) {
             throw new ServletException(
                     "The error occurred while create Ajax response. "
-                            + e.getMessage());
+                            + e.getMessage(), e);
         }
         String result = null;
         if (target instanceof String) {
