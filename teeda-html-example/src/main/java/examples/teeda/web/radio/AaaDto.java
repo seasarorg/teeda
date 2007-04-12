1569eb213ad4168fe0e19ffa1b48e70297509a40
shot6 <shinpei.ohtani@gmail.com>
2007-04-12 07:58:07 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3033 e0972215-f904-0410-97d3-99f68b247671

--- teeda-html-example/src/main/java/examples/teeda/web/radio/AaaDto.java
+++ teeda-html-example/src/main/java/examples/teeda/web/radio/AaaDto.java
@@ -9,13 +9,17 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
 package examples.teeda.web.radio;
 
-public class AaaDto {
+import java.io.Serializable;
+
+public class AaaDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	private String label;
 
