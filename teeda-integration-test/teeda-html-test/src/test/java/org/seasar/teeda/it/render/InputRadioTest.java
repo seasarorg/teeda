f227046f8b3bb8e6f31d747318cc9c3370f7dd7c
manhole <manholex@gmail.com>
2007-04-25 03:27:16 +0000

    https://www.seasar.org/issues/browse/TEEDA-290 JSPに続いてHTMLテンプレートの対応も完了。 testはintegration側で実施。
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3060 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/teeda-html-test/src/test/java/org/seasar/teeda/it/render/InputRadioTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it.render;
+
+import junit.framework.Test;
+
+import org.seasar.teeda.unit.web.TeedaWebTestCase;
+import org.seasar.teeda.unit.web.TeedaWebTester;
+
+/**
+ * @author manhole
+ */
+public class InputRadioTest extends TeedaWebTestCase {
+
+    public static Test suite() throws Exception {
+        return setUpTest(InputRadioTest.class);
+    }
+
+    public void testSelectValueAndSubmit() throws Exception {
+        // ## Arrange ##
+        TeedaWebTester tester = new TeedaWebTester();
+
+        // ## Act ##
+        tester.beginAt(getBaseUrl(), "view/radio/inputRadio.html");
+        tester.dumpHtml();
+
+        tester.assertRadioOptionSelectedByName("inputRadioForm:aaa", "3");
+        tester.assertTextEqualsById("aaa-display", "3");
+
+        tester.clickRadioOptionByName("inputRadioForm:aaa", "2");
+        tester.submitByName("inputRadioForm:doAction");
+        tester.dumpHtml();
+
+        // ## Assert ##
+        tester.assertRadioOptionSelectedByName("inputRadioForm:aaa", "2");
+        tester.assertTextEqualsById("aaa-display", "2");
+    }
+
+}
