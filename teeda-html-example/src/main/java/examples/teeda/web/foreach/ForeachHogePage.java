c0a079ab3265cd0624f594dbf5f10f1d8ee63f9d
shot6 <shinpei.ohtani@gmail.com>
2007-05-07 07:11:43 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3074 e0972215-f904-0410-97d3-99f68b247671

--- teeda-html-example/src/main/java/examples/teeda/web/foreach/ForeachHogePage.java
+++ teeda-html-example/src/main/java/examples/teeda/web/foreach/ForeachHogePage.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -23,11 +23,11 @@ import java.util.List;
  */
 public class ForeachHogePage extends AbstractForeachHogePage {
 
-	// public static final String num1_lengthValidator = "minimum=3";
+	public static final String num1_lengthValidator = "minimum=3";
 
-	// public static final String aaa_lengthValidator = "minimum=3";
+	public static final String aaa_lengthValidator = "minimum=3";
 
-	// public static final String bbb_lengthValidator = "minimum=3";
+	public static final String bbb_lengthValidator = "minimum=3";
 
 	// public static final String fooItems_MESSAGE_AGGREGATION =
 	// "id=examples.teeda.web.foreach.ForeachHoge.invaid";
