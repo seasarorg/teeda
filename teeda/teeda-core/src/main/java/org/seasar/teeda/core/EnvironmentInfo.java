80fa68b4f5c0427f9c99209bbc3d309f805f7e65
shot6 <shinpei.ohtani@gmail.com>
2007-05-14 04:43:48 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3077 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/EnvironmentInfo.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/EnvironmentInfo.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -42,7 +42,8 @@ public class EnvironmentInfo {
             if (key.length() > length) {
                 length = key.length();
             }
-            value = (String) properties.get(key);
+            Object object = properties.get(key);
+            value = object.toString();
             map.put(key, value);
         }
         for (Iterator itr = map.entrySet().iterator(); itr.hasNext();) {
