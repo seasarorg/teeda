4ab337eaaecdd310af8accd1c73746385b8231b8
mopemope <yutaka.matsubara@gmail.com>
2007-06-08 00:40:31 +0000

    AjaxSampleの追加
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3186 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/ajax/AjaxPage.java
@@ -0,0 +1,102 @@
+package examples.teeda.web.ajax;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AjaxPage {
+
+	private String sample1;
+
+	private List sample1Items;
+
+	private String sample2;
+
+	private List sample2Items;
+	
+	public Class initialize(){
+		if(this.sample1Items == null){
+			this.sample1Items = new ArrayList();
+			for(int i = 0; i < 3; i++){
+				SelectItemDto dto = new SelectItemDto();
+				dto.setLabel("事業所 "+Integer.toString(i));
+				dto.setValue(i);
+				this.sample1Items.add(dto);
+			}			
+		}
+		if(this.sample2Items == null){
+			this.sample2Items = new ArrayList();			
+		}
+		
+		return null;
+	}
+	
+	public String ajaxStartAjax() {
+		return "Start Ajax";
+	}
+
+	public NameDto ajaxStartAjaxObject() {
+		NameDto dto = new NameDto();
+		dto.setFirstName("foo");
+		dto.setLastName("bar");
+		return dto;
+	}
+
+	public Map ajaxStartAjaxList() {
+		List list = new ArrayList();
+		for (int i = 0; i < 10; i++) {
+			ListDto dto = new ListDto();
+			dto.setNo(i);
+			dto.setName("user" + Integer.toString(i));
+			list.add(dto);
+		}
+		Map map = new HashMap();
+		map.put("list", list);
+		return map;
+	}
+	
+	public List ajaxCombinationList(int no) {
+		List list = new ArrayList();
+		for(int i = 0; i < 5; i++){
+			SelectItemDto dto = new SelectItemDto();
+			dto.setLabel("事業所 "+Integer.toString(no)+"の部署 "+Integer.toString(i));
+			dto.setValue(i);
+			list.add(dto);			
+		}
+		return list;
+	}
+
+	public String getSample1() {
+		return sample1;
+	}
+
+	public void setSample1(String sample1) {
+		this.sample1 = sample1;
+	}
+
+	public String getSample2() {
+		return sample2;
+	}
+
+	public void setSample2(String sample2) {
+		this.sample2 = sample2;
+	}
+
+	public List getSample1Items() {
+		return sample1Items;
+	}
+
+	public void setSample1Items(List sample1Items) {
+		this.sample1Items = sample1Items;
+	}
+
+	public List getSample2Items() {
+		return sample2Items;
+	}
+
+	public void setSample2Items(List sample2Items) {
+		this.sample2Items = sample2Items;
+	}
+
+}
---
+++ teeda-html-example/src/main/java/examples/teeda/web/ajax/SelectItemDto.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.teeda.web.ajax;
+
+import java.io.Serializable;
+
+public class SelectItemDto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String label;
+
+	private int value;
+
+	private String nullLabel;
+	
+	public String getNullLabel() {
+		return nullLabel;
+	}
+
+	public void setNullLabel(String nullLabel) {
+		this.nullLabel = nullLabel;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
