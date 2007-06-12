f44a270d276641eecb5d3346bb6a02f6293d5af7
shot6 <shinpei.ohtani@gmail.com>
2007-06-12 07:01:57 +0000

    exampleとITにRedirectScopeのテストを追加。
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3211 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/scope/RedirectScopeResult1Page.java
@@ -0,0 +1,33 @@
+package examples.teeda.web.scope;
+
+public class RedirectScopeResult1Page {
+
+	private String message;
+
+	private String hoge;
+
+	public String getHoge() {
+		return hoge;
+	}
+
+	public void setHoge(String hoge) {
+		this.hoge = hoge;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String initialize() {
+		return null;
+	}
+
+	public String prerender() {
+		return null;
+	}
+
+}
