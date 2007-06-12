40a5970d242c502458259aee151435ae189794a6
shot6 <shinpei.ohtani@gmail.com>
2007-06-12 04:54:32 +0000

    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3209 e0972215-f904-0410-97d3-99f68b247671

--- teeda-tiger/src/main/java/org/seasar/teeda/extension/annotation/convert/BigDecimalConverter.java
+++ teeda-tiger/src/main/java/org/seasar/teeda/extension/annotation/convert/BigDecimalConverter.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -30,7 +30,10 @@ import java.lang.annotation.Target;
 public @interface BigDecimalConverter {
 
 	String pattern() default "";
-	
+
 	String target() default "";
-	
+
+	int scale() default -1;
+
+	int roundingMode() default -1;
 }
