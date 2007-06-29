3eb6e295ed37ce881b61dc7ff4f714dd8cfa1001
Yasuo Higa <higayasuo@gmail.com>
2007-06-29 06:51:30 +0000

    Seasar-user:8811対応。
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3268 e0972215-f904-0410-97d3-99f68b247671

--- teeda-html-example/src/main/java/examples/teeda/web/foreach/ForeachListMapPage.java
+++ teeda-html-example/src/main/java/examples/teeda/web/foreach/ForeachListMapPage.java
@@ -62,28 +62,4 @@ public class ForeachListMapPage {
 	public void setFoo(String foo) {
 		this.foo = foo;
 	}
-
-	public static class FooItem {
-
-		private String foo;
-
-		private String bar;
-
-		public String getBar() {
-			return bar;
-		}
-
-		public void setBar(String bar) {
-			this.bar = bar;
-		}
-
-		public String getFoo() {
-			return foo;
-		}
-
-		public void setFoo(String foo) {
-			this.foo = foo;
-		}
-	}
-
 }
