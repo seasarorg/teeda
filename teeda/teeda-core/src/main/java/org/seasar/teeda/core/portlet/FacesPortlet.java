bbf2e9c5634ac3934643e658f9b49e825748866d
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2007-07-17 05:51:08 +0000

    fixed https://www.seasar.org/issues/browse/TEEDA-334
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3343 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/portlet/FacesPortlet.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/portlet/FacesPortlet.java
@@ -16,9 +16,7 @@
 package org.seasar.teeda.core.portlet;
 
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -570,100 +568,6 @@ public class FacesPortlet extends GenericPortlet {
             String key = (String) enu.nextElement();
             response.setRenderParameter(key, request.getParameter(key));
         }
-    }
-
-    protected class FacesPortletState implements Serializable {
-
-        private static final long serialVersionUID = -2877915312956741979L;
-
-        private Map messages = new HashMap();
-
-        private Map requestMap = null;
-
-        private String viewId = null;
-
-        private Object state = null;
-
-        public void addMessage(String clientId, FacesMessage message) {
-            List list = (List) messages.get(clientId);
-            if (list == null) {
-                list = new ArrayList();
-                messages.put(clientId, list);
-            }
-            list.add(message);
-        }
-
-        public Iterator getMessages(String clientId) {
-            List list = (List) messages.get(clientId);
-            if (list != null) {
-                return (list.iterator());
-            } else {
-                return (Collections.EMPTY_LIST.iterator());
-            }
-        }
-
-        public StringBuffer getMessagesBuffer(String clientId) {
-            List list = (List) messages.get(clientId);
-            StringBuffer buffer = new StringBuffer();
-            if (list != null) {
-                Iterator messages = list.iterator();
-                FacesMessage message;
-                while (messages.hasNext()) {
-                    message = (FacesMessage) messages.next();
-                    buffer.append(" ");
-                    buffer.append(message.getDetail());
-                }
-            }
-            return buffer;
-        }
-
-        // iterate over the client ids in this view
-        public Iterator getClientIds() {
-            return (messages.keySet().iterator());
-        }
-
-        /**
-         * @return Returns the requestMap.
-         */
-        public Map getRequestMap() {
-            return requestMap;
-        }
-
-        /**
-         * @param requestMap The requestMap to set.
-         */
-        public void setRequestMap(Map requestMap) {
-            this.requestMap = requestMap;
-        }
-
-        /**
-         * @return Returns the viewId.
-         */
-        public String getViewId() {
-            return viewId;
-        }
-
-        /**
-         * @param viewId The viewId to set.
-         */
-        public void setViewId(String viewId) {
-            this.viewId = viewId;
-        }
-
-        /**
-         * @return Returns the state.
-         */
-        public Object getState() {
-            return state;
-        }
-
-        /**
-         * @param state The state to set.
-         */
-        public void setState(Object state) {
-            this.state = state;
-        }
-
     }
 
 }
