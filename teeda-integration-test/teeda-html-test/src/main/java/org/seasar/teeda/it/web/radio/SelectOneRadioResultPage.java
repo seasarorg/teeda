6d350d102ccff167a0071ed4272a141869c258ea
shot6 <shinpei.ohtani@gmail.com>
2007-07-31 10:08:02 +0000

    TEEDA-325 SelectOneMenuのようにSelect系コンポーネントもLabelを引き継ぎたい
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3383 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/teeda-html-test/src/main/java/org/seasar/teeda/it/web/radio/SelectOneRadioResultPage.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it.web.radio;
+
+import java.util.List;
+
+/**
+ * @author shot
+ */
+public class SelectOneRadioResultPage {
+
+	private Integer aaa;
+
+	private Integer bbb;
+
+	private List aaaItems;
+
+	private String aaaLabel;
+
+	public String getAaaLabel() {
+		return aaaLabel;
+	}
+
+	public void setAaaLabel(String aaaLabel) {
+		this.aaaLabel = aaaLabel;
+	}
+
+	public String initialize() {
+		return null;
+	}
+
+	public Integer getAaa() {
+		return aaa;
+	}
+
+	public void setAaa(Integer aaa) {
+		this.aaa = aaa;
+	}
+
+	public List getAaaItems() {
+		return aaaItems;
+	}
+
+	public void setAaaItems(List aaaItems) {
+		this.aaaItems = aaaItems;
+	}
+
+	public Integer getBbb() {
+		return bbb;
+	}
+
+	public void setBbb(Integer bbb) {
+		this.bbb = bbb;
+	}
+
+	public void prerender() {
+		System.out.println("aaaLabel = " + aaaLabel);
+	}
+}
