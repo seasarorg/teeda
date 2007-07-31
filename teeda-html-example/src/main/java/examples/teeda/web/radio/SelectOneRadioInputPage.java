5862ac5ab5347b7ee8cdcf5641f60daa3f9af9a0
shot6 <shinpei.ohtani@gmail.com>
2007-07-31 10:09:51 +0000

    TEEDA-325 SelectOneMenuのようにSelect系コンポーネントもLabelを引き継ぎたい
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3384 e0972215-f904-0410-97d3-99f68b247671

--- teeda-html-example/src/main/java/examples/teeda/web/radio/SelectOneRadioInputPage.java
+++ teeda-html-example/src/main/java/examples/teeda/web/radio/SelectOneRadioInputPage.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -30,6 +30,16 @@ public class SelectOneRadioInputPage {
 	private List aaaItems;
 
 	private Integer bbb;
+
+	private String aaaLabel;
+
+	public String getAaaLabel() {
+		return aaaLabel;
+	}
+
+	public void setAaaLabel(String aaaLabel) {
+		this.aaaLabel = aaaLabel;
+	}
 
 	public String initialize() {
 		aaaItems = new ArrayList();
--- teeda-html-example/src/main/java/examples/teeda/web/radio/SelectOneRadioResultPage.java
+++ teeda-html-example/src/main/java/examples/teeda/web/radio/SelectOneRadioResultPage.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
@@ -25,8 +25,18 @@ public class SelectOneRadioResultPage {
 	private Integer aaa;
 
 	private Integer bbb;
-	
+
 	private List aaaItems;
+
+	private String aaaLabel;
+
+	public String getAaaLabel() {
+		return aaaLabel;
+	}
+
+	public void setAaaLabel(String aaaLabel) {
+		this.aaaLabel = aaaLabel;
+	}
 
 	public String initialize() {
 		return null;
@@ -56,4 +66,7 @@ public class SelectOneRadioResultPage {
 		this.bbb = bbb;
 	}
 
+	public void prerender() {
+		System.out.println("aaaLabel = " + aaaLabel);
+	}
 }
