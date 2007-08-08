9f3fe8b379c57095ebf05f1da2bd3d97d7c9b93d
Koichi Kobayashi <koichik@improvement.jp>
2007-08-08 11:54:14 +0000

    リダイレクトのサンプルを追加しました．
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3418 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-html-example/src/main/java/examples/teeda/web/redirect/RedirectPage.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package examples.teeda.web.redirect;
+
+import java.io.IOException;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+
+public class RedirectPage {
+
+    public static final String url_TRequiredValidator = null;
+
+    private HttpServletResponse response;
+
+    private FacesContext facesContext;
+
+    private String url;
+
+    public void setResponse(HttpServletResponse response) {
+        this.response = response;
+    }
+
+    public void setFacesContext(FacesContext facesContext) {
+        this.facesContext = facesContext;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String doRedirect() throws IOException {
+        response.sendRedirect(url);
+        facesContext.responseComplete();
+        return null;
+    }
+
+}
