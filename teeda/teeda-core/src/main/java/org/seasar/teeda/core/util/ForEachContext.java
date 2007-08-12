7b8c2b4e029bd79c47c48fdcb4eec0f64cdc42b2
manhole <manholex@gmail.com>
2007-08-12 15:10:11 +0000

    コミット漏れorz
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3453 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/util/ForEachContext.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.core.util;
+
+/**
+ * @author manhole
+ */
+public class ForEachContext {
+
+    private static ThreadLocal threadLocal = new ThreadLocal();
+
+    private int depth;
+
+    public static ForEachContext getContext() {
+        ForEachContext context = (ForEachContext) threadLocal.get();
+        if (context == null) {
+            context = new ForEachContext();
+            threadLocal.set(context);
+        }
+        return context;
+    }
+
+    public void begin() {
+        depth++;
+    }
+
+    public void end() {
+        depth--;
+        if (depth <= 0) {
+            clear();
+        }
+    }
+
+    public void clear() {
+        depth = 0;
+        threadLocal.set(null);
+    }
+
+    public boolean isInForEach() {
+        if (0 < depth) {
+            return true;
+        }
+        return false;
+    }
+
+}
