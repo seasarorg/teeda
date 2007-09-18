19e45253fe8e6ed755f20d420c806943f72ee8b5
shot6 <shinpei.ohtani@gmail.com>
2007-09-18 05:51:24 +0000

    -tiger前提にした -TEEDA-360対応
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3587 e0972215-f904-0410-97d3-99f68b247671

---
+++ teeda-integration-test/teeda-html-test/src/main/java/org/seasar/teeda/it/web/condition/AaaPage.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2004-2006 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it.web.condition;
+
+import java.util.Date;
+
+import javax.faces.internal.PhaseUtil;
+
+import org.seasar.teeda.extension.annotation.validator.Required;
+
+/**
+ * @author shot
+ */
+public class AaaPage {
+
+	private boolean dayReport;
+
+	@Required
+	private Date inYearMonth;
+
+	@Required
+	private Date inYearMonthDayFrom;
+
+	private Date inYearMonthDayTo;
+
+	/**
+	 * @return
+	 */
+	public Class initialize() {
+		dayReport = true;
+		return null;
+	}
+
+	/**
+	 * @return
+	 */
+	public Class prerender() {
+		return null;
+	}
+
+	public boolean isDayReport() {
+		return dayReport;
+	}
+
+	public void setDayReport(boolean dayReport) {
+		this.dayReport = dayReport;
+		System.out.println("dayReport = " + dayReport);
+		System.out.println("phase = " + PhaseUtil.getCurrentPhase());
+	}
+
+	public Date getInYearMonth() {
+		return inYearMonth;
+	}
+
+	public void setInYearMonth(Date inYearMonth) {
+		this.inYearMonth = inYearMonth;
+	}
+
+	public Date getInYearMonthDayFrom() {
+		return inYearMonthDayFrom;
+	}
+
+	public void setInYearMonthDayFrom(Date inYearMonthDayFrom) {
+		this.inYearMonthDayFrom = inYearMonthDayFrom;
+	}
+
+	public Date getInYearMonthDayTo() {
+		return inYearMonthDayTo;
+	}
+
+	public void setInYearMonthDayTo(Date inYearMonthDayTo) {
+		this.inYearMonthDayTo = inYearMonthDayTo;
+	}
+
+	public String getLayout() {
+		return null;
+	}
+
+	public Class doExecute() {
+		return null;
+	}
+
+}
---
+++ teeda-integration-test/teeda-html-test/src/test/java/org/seasar/teeda/it/render/Teeda360Test.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.teeda.it.render;
+
+import junit.framework.Test;
+
+import org.seasar.teeda.unit.web.TeedaWebTestCase;
+import org.seasar.teeda.unit.web.TeedaWebTester;
+
+/**
+ * @author shot
+ * 
+ * https://www.seasar.org/issues/browse/TEEDA-360
+ */
+public class Teeda360Test extends TeedaWebTestCase {
+
+	public static Test suite() throws Exception {
+		return setUpTest(Teeda360Test.class);
+	}
+
+	public void testRender() throws Exception {
+		try {
+			// ## Arrange ##
+			TeedaWebTester tester = new TeedaWebTester();
+
+			// ## Act ##
+			tester.beginAt(getBaseUrl(), "view/condition/aaa.html");
+			tester.dumpHtml();
+
+			// ## Assert ##
+			tester.assertTextNotPresent("isNotDayReport");
+
+			tester.setTextById("inYearMonthDayFrom", "aaa");
+			tester.setTextById("inYearMonthDayTo", "aaa");
+
+			tester.submitById("doExecute");
+
+			tester.assertTextEqualsById("inYearMonthDayFrom", "aaa");
+			tester.assertTextEqualsById("inYearMonthDayTo", "aaa");
+			tester.assertTextNotPresent("isNotDayReport");
+		} catch (Throwable e) {
+			fail();
+		}
+	}
+
+}
