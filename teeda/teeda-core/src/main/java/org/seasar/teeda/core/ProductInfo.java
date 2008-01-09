8f12c8fb9e8b110652681790c06c949e672e9090
Koichi Kobayashi <koichik@improvement.jp>
2008-01-09 10:18:00 +0000

    ProductInfo のバージョンを更新していなかったので修正． META-INF とか Maven が入れてくれる情報を出すようにした方がいいんじゃないかなぁ．
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3841 e0972215-f904-0410-97d3-99f68b247671

--- teeda/teeda-core/src/main/java/org/seasar/teeda/core/ProductInfo.java
+++ teeda/teeda-core/src/main/java/org/seasar/teeda/core/ProductInfo.java
@@ -25,7 +25,7 @@ public class ProductInfo {
 
     private static final String MINOR_VERSION = "0";
 
-    private static final String LOCAL_VERSION = "11";
+    private static final String LOCAL_VERSION = "12";
 
     private static boolean isSnapshot = false;
 
@@ -34,8 +34,8 @@ public class ProductInfo {
     }
 
     public static String getVersion() {
-        return MAJOR_VERSION + "." + MINOR_VERSION + "." + LOCAL_VERSION
-                + ((isSnapshot) ? "-SNAPSHOT" : "");
+        return MAJOR_VERSION + "." + MINOR_VERSION + "." + LOCAL_VERSION +
+                ((isSnapshot) ? "-SNAPSHOT" : "");
     }
 
     /*
