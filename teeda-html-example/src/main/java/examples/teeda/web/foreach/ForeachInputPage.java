b887da0e0a3a328988f663bb56ee85cebd483116
Koichi Kobayashi <koichik@improvement.jp>
2008-01-18 08:10:24 +0000

    [Seasar-user:12195] を調査した際に S2Dao に依存するようにしたのを間違ってそのままコミットしていたので戻しました．心より恥じる．
    git-svn-id: https://www.seasar.org/svn/teeda/trunk@3879 e0972215-f904-0410-97d3-99f68b247671

--- teeda-html-example/src/main/java/examples/teeda/web/foreach/ForeachInputPage.java
+++ teeda-html-example/src/main/java/examples/teeda/web/foreach/ForeachInputPage.java
@@ -19,116 +19,73 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.seasar.dao.pager.DefaultPagerCondition;
-import org.seasar.dao.pager.PagerUtil;
-import org.seasar.teeda.extension.annotation.scope.PageScope;
-
 public class ForeachInputPage {
 
-	private static final int ROWLIMIT = 5;
-	private static final int RECOARDS = 50;
+	private FooDto[] fooItems;
 
-	@PageScope
-	public List<Foo> targetItems;
+	public String doNothing() {
+		return null;
+	}
 
+	public String initialize() {
+		List l = new ArrayList();
+		{
+			FooDto f = new FooDto();
+			f.setFooNo(new Integer(1));
+			f.setAaa("aa1");
+			f.setBbb("bb1");
+			l.add(f);
+		}
+		{
+			FooDto f = new FooDto();
+			f.setFooNo(new Integer(2));
+			f.setAaa("aa2");
+			f.setBbb("bb2");
+			l.add(f);
+		}
+		{
+			FooDto f = new FooDto();
+			f.setFooNo(new Integer(3));
+			f.setAaa("aa3");
+			f.setBbb("bb3");
+			l.add(f);
+		}
+		fooItems = new FooDto[3];
+		l.toArray(fooItems);
+
+		return null;
+	}
+
+	public String prerender() {
+		return null;
+	}
+
+	// public Foo getFoo() {
+	// return foo;
+	// }
+	//
+	// public void setFoo(Foo foo) {
+	// this.foo = foo;
+	// }
+
+	public FooDto[] getFooItems() {
+		return fooItems;
+	}
+
+	public void setFooItems(FooDto[] fooItems) {
+		this.fooItems = fooItems;
+	}
+
+	private Integer fooNo;
 	private String aaa;
-	private List<Foo> fooItems;
-	public int find_count;
-	public int current_page;
-	public int total_page;
+	private String bbb;
 
-	public Class initialize() {
-		if (targetItems == null) {
-			int count = RECOARDS;
-			targetItems = new ArrayList<Foo>();
-			for (int i = 1; i <= count; i++) {
-				Foo item = new Foo();
-				item.setAaa("a" + String.valueOf(i));
-				targetItems.add(item);
-			}
-		}
-		current_page = 1;
-		return null;
+	public Integer getFooNo() {
+		return fooNo;
 	}
 
-	public Class prerender() {
-		find_count = targetItems.size();
-		current_page = find_count == 0 ? 0 : current_page;
-		current_page = current_page <= 0 ? 1 : current_page;
-		current_page = current_page > getTotal_page() ? getTotal_page()
-				: current_page;
-
-		DefaultPagerCondition condition = new DefaultPagerCondition();
-		condition.setLimit(ROWLIMIT);
-		condition.setOffset((current_page - 1) * ROWLIMIT);
-		fooItems = PagerUtil.filter(targetItems, condition);
-
-		System.out.println("current_page:" + current_page);
-		for (int i = 0; i < fooItems.size(); i++) {
-			Foo array_element = fooItems.get(i);
-			System.out.println(array_element);
-		}
-		return null;
-	}
-
-	public String doPageFirst() {
-		current_page = 1;
-		return null;
-	}
-
-	public String doPagePrv() {
-		current_page = current_page > 1 ? current_page - 1 : 1;
-		return null;
-	}
-
-	public String doPageMove() {
-		return null;
-	}
-
-	public String doPageNext() {
-		current_page = current_page >= getTotal_page() - 1 ? getTotal_page()
-				: current_page + 1;
-		return null;
-	}
-
-	public String doPageFinal() {
-		current_page = getTotal_page();
-		return null;
-	}
-
-	public int getTotal_page() {
-		int result = 0;
-		result = find_count == 0 ? 0 : (int) (find_count / ROWLIMIT)
-				+ ((find_count % ROWLIMIT) == 0 ? 0 : 1);
-		return result;
-	}
-
-	public String doTest() {
-		System.out.println("=== 入力値 ===");
-		for (int i = 0; i < fooItems.size(); i++) {
-			Foo array_element = fooItems.get(i);
-			System.out.println(array_element);
-		}
-		return null;
-	}
-
-	public static class Foo implements Serializable {
-
-		private static final long serialVersionUID = 1L;
-
-		private String aaa;
-
-		public String getAaa() {
-			return aaa;
-		}
-
-		public void setAaa(String aaa) {
-			this.aaa = aaa;
-		}
-
-		public String toString() {
-			return "[" + aaa + "]";
-		}
+	public void setFooNo(Integer no) {
+		this.fooNo = no;
 	}
 
 	public String getAaa() {
@@ -139,12 +96,44 @@ public class ForeachInputPage {
 		this.aaa = aaa;
 	}
 
-	public List<Foo> getFooItems() {
-		return fooItems;
+	public String getBbb() {
+		return bbb;
 	}
 
-	public void setFooItems(List<Foo> fooItems) {
-		this.fooItems = fooItems;
+	public void setBbb(String bbb) {
+		this.bbb = bbb;
 	}
 
+	public static class FooDto implements Serializable {
+
+		private static final long serialVersionUID = 1L;
+		private Integer fooNo;
+		private String aaa;
+		private String bbb;
+
+		public Integer getFooNo() {
+			return fooNo;
+		}
+
+		public void setFooNo(Integer no) {
+			this.fooNo = no;
+		}
+
+		public String getAaa() {
+			return aaa;
+		}
+
+		public void setAaa(String aaa) {
+			this.aaa = aaa;
+		}
+
+		public String getBbb() {
+			return bbb;
+		}
+
+		public void setBbb(String bbb) {
+			this.bbb = bbb;
+		}
+
+	}
 }
